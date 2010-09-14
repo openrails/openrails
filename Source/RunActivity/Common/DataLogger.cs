4053995fbc111ca4ea6c5bcb993e41599dcac050
sarosigy <sarosigy@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2010-09-14 08:17:10 +0000

    Updated DataLogger with a huge cache and a destructor which also flushes to disk
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@353 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/Common/DataLogger.cs
+++ Source/RunActivity/Common/DataLogger.cs
@@ -8,42 +8,54 @@ namespace ORTS
 {
     class DataLogger
     {
-		readonly int CacheSize;
-		readonly StringBuilder Cache = new StringBuilder();
-		int CacheCount = 0;
-		bool FirstItem = true;
+        readonly int CacheSize;
+        const int LargeCacheSize = 2048 * 1024;  // 2 Megs
+        readonly StringBuilder Cache = new StringBuilder();
+        int CacheCount = 0;
+        bool FirstItem = true;
 
         public DataLogger(int cacheSize)
         {
-			CacheSize = cacheSize;
+            CacheSize = cacheSize;
+            Cache.EnsureCapacity(LargeCacheSize);
         }
 
         public void Data(string data)
         {
-			if (!FirstItem)
-				Cache.Append(',');
-			Cache.Append(data);
-			FirstItem = false;
-		}
+            if (!FirstItem)
+                Cache.Append(',');
+            Cache.Append(data);
+            FirstItem = false;
+        }
 
-		public void End()
-		{
-			Cache.AppendLine();
+        public void End()
+        {
+            Cache.AppendLine();
+            /*
 			if (++CacheCount >= CacheSize)
 				Flush();
-			FirstItem = true;
-		}
+             */
+            if (Cache.Length > LargeCacheSize)
+                Flush();
+            FirstItem = true;
+        }
 
         public void Flush()
         {
-			//TODO: this whole function should be in a thread maybe
-			using (StreamWriter file = File.AppendText("dump.csv"))
-			{
-				file.Write(Cache);
-			    file.Close();
-			}
-			Cache.Length = 0;
-			CacheCount = 0;
-		}
+            //TODO: this whole function should be in a thread maybe
+            using (StreamWriter file = File.AppendText("dump.csv"))
+            {
+                file.Write(Cache);
+                file.Close();
+            }
+            Cache.Length = 0;
+            CacheCount = 0;
+            Cache.EnsureCapacity(LargeCacheSize);
+        }
+
+        ~DataLogger()
+        {
+            Flush();
+        }
     }
 }
