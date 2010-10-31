efbe62cec5d2a437604d129bc57d24eb4705ac41
interrobang <interrobang@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2010-10-31 02:24:02 +0000

    Added DataLoggerEx.cs to RunActivity/Common.  The DataLoggerEX class allows a user to have multiple loggers with different file names.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@437 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Source/RunActivity/Common/DataLoggerEx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ORTS
+{
+    class DataLoggerEx
+    {
+
+        const int m_CacheSize = 2048 * 1024;  // 2 Megs
+        readonly StringBuilder m_Cache = new StringBuilder(m_CacheSize);
+        readonly string m_fileName;
+        bool m_isActive;
+
+        bool FirstItem = true;
+
+        public DataLoggerEx()
+        {
+            m_fileName = "dump.txt";
+            m_isActive = true;
+        }
+        
+        public DataLoggerEx(string fileName)
+        {
+            m_fileName = fileName;
+            m_isActive = true;
+        }
+
+        ~DataLoggerEx()
+        {
+            Flush();
+        }
+
+        public void Start()
+        {
+            m_isActive = true;
+        }
+
+        public void Stop()
+        {
+            Flush();
+            m_isActive = false;
+        }
+
+        public bool IsActive
+        {
+            get
+            {
+                return m_isActive;
+            }
+        }
+
+        public void Data(string data)
+        {
+            if (m_isActive)
+            {
+                if (!FirstItem)
+                    m_Cache.Append(',');
+                m_Cache.Append(data);
+                FirstItem = false;
+            }
+        }
+
+        // Synonym for End() with a more descriptive name
+        public void EndLine()
+        {
+            End();
+        }
+
+        public void End()
+        {
+            if (m_isActive)
+            {
+                m_Cache.AppendLine();
+			    if (m_Cache.Length >= m_CacheSize)
+				    Flush();
+                FirstItem = true;
+            }
+        }
+
+        public void Flush()
+        {
+            //TODO: this whole function should be in a thread maybe
+            using (StreamWriter file = File.AppendText(m_fileName))
+            {
+                file.Write(m_Cache);
+                file.Close();
+            }
+            m_Cache.Length = 0;
+        }
+    }
+}
