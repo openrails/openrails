83c7a8d111933e1eec2a506dcc7de16d39f02de0
CommanderMath <CommanderMath@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2010-12-03 16:06:46 +0000

    Added files needed for getting a 2D debug viewer working.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@473 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Source/RunActivity/Debug/DebugViewerForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ORTS.Debug
+{
+   public partial class DebugViewerForm : Form
+   {
+      public DebugViewerForm()
+      {
+         InitializeComponent();
+      }
+   }
+}
