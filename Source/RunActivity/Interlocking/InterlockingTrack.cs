09b9fa27e3283a090aeab85ea3db6093c7791b2b
CommanderMath <CommanderMath@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2010-12-04 02:07:49 +0000

    Added template for a track circuit object for interlocking purposes.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@479 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Source/RunActivity/Interlocking/InterlockingTrack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORTS.Interlocking
+{
+
+   /// <summary>
+   /// Defines an abstraction of a track circuit used within the interlocking system.
+   /// </summary>
+   public class InterlockingTrack
+   {
+      
+      /// <summary>
+      /// True when the track is occupied, false otherwise.
+      /// </summary>
+      public bool Occupied { get; set; }
+
+
+      /// <summary>
+      /// Reference to the simulation object.
+      /// </summary>
+      private Simulator simulator;
+
+
+      /// <summary>
+      /// TODO: create instances of tracks from existing track objects.
+      /// </summary>
+      public InterlockingTrack(Simulator simulator/*TrackItem foo*/)
+      {
+
+      }
+
+   }
+}
