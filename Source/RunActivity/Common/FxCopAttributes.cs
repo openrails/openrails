f6909172633e5d79f5554a0fd4217c06d56635ea
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2010-12-04 15:36:00 +0000

    Initial part of threading checks for OR:  - ThreadNameAttribute can be set on any method and will set the thread on which to assume it runs. Only one thread may be specified for any given method.  - CallOnThreadAttribute can be set on any class, constructor or method to say it is allowed to be called on a given thread. May be specified multiple times. Setting on a class defines allowed threads for all methods without their own attributes; method attributes completely replace class attributes. Overriding a method will inherit the base method's threading attributes and trying to redefine them is an error.
    The FxCop rules I've written (not public just yet) check that all callers of methods with CallOnThread set are allowed by the threading attributes; i.e. callee's allowed threads must be the same or a superset of the sum of all calling threads. Methods without CallOnThread set are not checked in any way.
    
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@482 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Source/RunActivity/Common/FxCopAttributes.cs
@@ -0,0 +1,52 @@
+﻿// COPYRIGHT 2010 by the Open Rails project.
+// This code is provided to enable you to contribute improvements to the open rails program.  
+// Use of the code for any other purpose or distribution of the code to anyone else
+// is prohibited without specific written permission from admin@openrails.org.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORTS
+{
+	/// <summary>
+	/// Explicitly sets the name of the thread on which the target will run.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+	sealed class ThreadNameAttribute : Attribute
+	{
+		readonly string threadName;
+
+		// This is a positional argument
+		public ThreadNameAttribute(string threadName)
+		{
+			this.threadName = threadName;
+		}
+
+		public string ThreadName
+		{
+			get { return threadName; }
+		}
+	}
+
+	/// <summary>
+	/// Defines a thread on which the target is allowed to run; multiple threads may be allowed for a single target.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+	sealed class CallOnThreadAttribute : Attribute
+	{
+		readonly string threadName;
+
+		// This is a positional argument
+		public CallOnThreadAttribute(string threadName)
+		{
+			this.threadName = threadName;
+		}
+
+		public string ThreadName
+		{
+			get { return threadName; }
+		}
+	}
+}
