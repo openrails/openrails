ea33cef351b74f5fc8bc79d2de55c63600228dae
CommanderMath <CommanderMath@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2010-12-06 06:43:55 +0000

    Progress made with debug viewer - basic tracks, switches visible. Simple UI navigation with buttons.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@484 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/Debugging/DebugViewerForm.Designer.cs
+++ Source/RunActivity/Debugging/DebugViewerForm.Designer.cs
@@ -29,33 +29,191 @@
       private void InitializeComponent()
       {
          this.pictureBox1 = new System.Windows.Forms.PictureBox();
+         this.refreshButton = new System.Windows.Forms.Button();
+         this.leftButton = new System.Windows.Forms.Button();
+         this.rightButton = new System.Windows.Forms.Button();
+         this.upButton = new System.Windows.Forms.Button();
+         this.downButton = new System.Windows.Forms.Button();
+         this.windowSizeUpDown = new System.Windows.Forms.NumericUpDown();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.showSwitches = new System.Windows.Forms.CheckBox();
          ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.windowSizeUpDown)).BeginInit();
          this.SuspendLayout();
          // 
          // pictureBox1
          // 
-         this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+         this.pictureBox1.Location = new System.Drawing.Point(12, 120);
          this.pictureBox1.Name = "pictureBox1";
-         this.pictureBox1.Size = new System.Drawing.Size(783, 490);
+         this.pictureBox1.Size = new System.Drawing.Size(512, 512);
          this.pictureBox1.TabIndex = 0;
          this.pictureBox1.TabStop = false;
+         // 
+         // refreshButton
+         // 
+         this.refreshButton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.refreshButton.Location = new System.Drawing.Point(37, 18);
+         this.refreshButton.Name = "refreshButton";
+         this.refreshButton.Size = new System.Drawing.Size(75, 23);
+         this.refreshButton.TabIndex = 1;
+         this.refreshButton.Text = "Refresh";
+         this.refreshButton.UseVisualStyleBackColor = true;
+         this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+         // 
+         // leftButton
+         // 
+         this.leftButton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.leftButton.Location = new System.Drawing.Point(134, 12);
+         this.leftButton.Name = "leftButton";
+         this.leftButton.Size = new System.Drawing.Size(35, 35);
+         this.leftButton.TabIndex = 2;
+         this.leftButton.Text = "L";
+         this.leftButton.UseVisualStyleBackColor = true;
+         this.leftButton.MouseLeave += new System.EventHandler(this.leftButton_MouseLeave);
+         this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+         this.leftButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.leftButton_MouseDown);
+         this.leftButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.leftButton_MouseUp);
+         // 
+         // rightButton
+         // 
+         this.rightButton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.rightButton.Location = new System.Drawing.Point(216, 12);
+         this.rightButton.Name = "rightButton";
+         this.rightButton.Size = new System.Drawing.Size(35, 35);
+         this.rightButton.TabIndex = 3;
+         this.rightButton.Text = "R";
+         this.rightButton.UseVisualStyleBackColor = true;
+         this.rightButton.MouseLeave += new System.EventHandler(this.rightButton_MouseLeave);
+         this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+         this.rightButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rightButton_MouseDown);
+         this.rightButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.rightButton_MouseUp);
+         // 
+         // upButton
+         // 
+         this.upButton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.upButton.Location = new System.Drawing.Point(175, 12);
+         this.upButton.Name = "upButton";
+         this.upButton.Size = new System.Drawing.Size(35, 35);
+         this.upButton.TabIndex = 4;
+         this.upButton.Text = "U";
+         this.upButton.UseVisualStyleBackColor = true;
+         this.upButton.MouseLeave += new System.EventHandler(this.upButton_MouseLeave);
+         this.upButton.Click += new System.EventHandler(this.upButton_Click);
+         this.upButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.upButton_MouseDown);
+         this.upButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.upButton_MouseUp);
+         // 
+         // downButton
+         // 
+         this.downButton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.downButton.Location = new System.Drawing.Point(257, 12);
+         this.downButton.Name = "downButton";
+         this.downButton.Size = new System.Drawing.Size(35, 35);
+         this.downButton.TabIndex = 5;
+         this.downButton.Text = "D";
+         this.downButton.UseVisualStyleBackColor = true;
+         this.downButton.MouseLeave += new System.EventHandler(this.downButton_MouseLeave);
+         this.downButton.Click += new System.EventHandler(this.downButton_Click);
+         this.downButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.downButton_MouseDown);
+         this.downButton.MouseUp += new System.Windows.Forms.MouseEventHandler(this.downButton_MouseUp);
+         // 
+         // windowSizeUpDown
+         // 
+         this.windowSizeUpDown.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.windowSizeUpDown.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+         this.windowSizeUpDown.Location = new System.Drawing.Point(318, 24);
+         this.windowSizeUpDown.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+         this.windowSizeUpDown.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+         this.windowSizeUpDown.Name = "windowSizeUpDown";
+         this.windowSizeUpDown.Size = new System.Drawing.Size(79, 23);
+         this.windowSizeUpDown.TabIndex = 6;
+         this.windowSizeUpDown.Value = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+         this.windowSizeUpDown.ValueChanged += new System.EventHandler(this.windowSizeUpDown_ValueChanged);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(315, 8);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(57, 13);
+         this.label1.TabIndex = 7;
+         this.label1.Text = "Resolution";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label2.Location = new System.Drawing.Point(403, 31);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(54, 16);
+         this.label2.TabIndex = 8;
+         this.label2.Text = "metres";
+         // 
+         // showSwitches
+         // 
+         this.showSwitches.AutoSize = true;
+         this.showSwitches.Checked = true;
+         this.showSwitches.CheckState = System.Windows.Forms.CheckState.Checked;
+         this.showSwitches.Location = new System.Drawing.Point(318, 63);
+         this.showSwitches.Name = "showSwitches";
+         this.showSwitches.Size = new System.Drawing.Size(160, 17);
+         this.showSwitches.TabIndex = 9;
+         this.showSwitches.Text = "Show Switches (Black Dots)";
+         this.showSwitches.UseVisualStyleBackColor = true;
+         this.showSwitches.CheckedChanged += new System.EventHandler(this.showSwitches_CheckedChanged);
          // 
          // DebugViewerForm
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.AutoScroll = true;
-         this.ClientSize = new System.Drawing.Size(889, 562);
+         this.ClientSize = new System.Drawing.Size(650, 644);
+         this.Controls.Add(this.showSwitches);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.windowSizeUpDown);
+         this.Controls.Add(this.downButton);
+         this.Controls.Add(this.upButton);
+         this.Controls.Add(this.rightButton);
+         this.Controls.Add(this.leftButton);
+         this.Controls.Add(this.refreshButton);
          this.Controls.Add(this.pictureBox1);
          this.Name = "DebugViewerForm";
          this.Text = "DebugViewerForm";
          ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.windowSizeUpDown)).EndInit();
          this.ResumeLayout(false);
+         this.PerformLayout();
 
       }
 
       #endregion
 
       private System.Windows.Forms.PictureBox pictureBox1;
+      private System.Windows.Forms.Button refreshButton;
+      private System.Windows.Forms.Button leftButton;
+      private System.Windows.Forms.Button rightButton;
+      private System.Windows.Forms.Button upButton;
+      private System.Windows.Forms.Button downButton;
+      private System.Windows.Forms.NumericUpDown windowSizeUpDown;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.CheckBox showSwitches;
    }
 }
--- Source/RunActivity/Debugging/DebugViewerForm.cs
+++ Source/RunActivity/Debugging/DebugViewerForm.cs
@@ -23,20 +23,55 @@ namespace ORTS.Debugging
       /// </summary>
       private readonly Simulator simulator;
 
+      /// <summary>
+      /// Reference to the Viewer3D object.
+      /// </summary>
       private readonly Viewer3D viewer;
 
-      Timer timer = new Timer();
-      
+
+      private int IM_Width = 512;
+      private int IM_Height = 512;
+
+      /// <summary>
+      /// True when the user has the "Move left" pressed.
+      /// </summary>
+      private bool LeftButtonDown = false;
+
+      /// <summary>
+      /// True when the user has the "Move right" pressed.
+      /// </summary>
+      private bool RightButtonDown = false;
+
+      /// <summary>
+      /// True when the user has the "Move up" pressed.
+      /// </summary>
+      private bool UpButtonDown = false;
+
+      /// <summary>
+      /// True when the user has the "Move down" pressed.
+      /// </summary>
+      private bool DownButtonDown = false;
+
+
+       /// <summary>
+      /// Defines the area to view, in meters.
+      /// </summary>
+      private RectangleF ViewWindow;
+
+      /// <summary>
+      /// Used to periodically check if we should shift the view when the
+      /// user is holding down a "shift view" button.
+      /// </summary>
+      private Timer UITimer;
 
       /// <summary>
       /// Creates a new DebugViewerForm.
       /// </summary>
       /// <param name="simulator"></param>
+      /// /// <param name="viewer"></param>
       public DebugViewerForm(Simulator simulator, Viewer3D viewer)
       {
          InitializeComponent();
-
-
 
          if (simulator == null)
          {
@@ -47,151 +82,392 @@ namespace ORTS.Debugging
 
          this.simulator = simulator;
 
-         
 
-         timer.Tick += new EventHandler(timer_Tick);
-         timer.Interval = 1000;
-         timer.Start();
+         // initialise the timer used to handle user input
+         UITimer = new Timer();
+         UITimer.Interval = 100;
+         UITimer.Tick += new EventHandler(UITimer_Tick);
+         UITimer.Start();
 
+         ViewWindow = new RectangleF(0, 0, 5000f, 5000f);
+         windowSizeUpDown.Accelerations.Add(new NumericUpDownAcceleration(1, 100));
 
-         pictureBox1.Width = 4000;
-         pictureBox1.Height = 6000;   
-         pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-         
-      }
+         InitImage();
 
-      void timer_Tick(object sender, EventArgs e)
-      {
-         GenerateView();
       }
 
 
-      private TDBTraveller GetFrontTraveller()
+      /// <summary>
+      /// When the user holds down the  "L", "R", "U", "D" buttons,
+      /// shift the view. Avoids the case when the user has to click
+      /// buttons like crazy.
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      void UITimer_Tick(object sender, EventArgs e)
       {
-         // for now, assume player train is FIRST train
-         return new TDBTraveller(simulator.Trains[0].FrontTDBTraveller);
-      }
-
-
-      private float scale = 0.1f;
-
-      private float shiftX = 500f;
-
-      private void GenerateView()
-      {
-         TDBTraveller t = GetFrontTraveller();
-
-         //t.TN
-
-         float dotSize = 60f * scale;
-
-         using(Graphics g = Graphics.FromImage(pictureBox1.Image))
+         if (DownButtonDown)
          {
+            ShiftViewDown();
+         }
 
-            g.Clear(Color.Transparent);
+         if (UpButtonDown)
+         {
+            ShiftViewUp();
+         }
 
-            foreach (var trackNode in simulator.TDB.TrackDB.TrackNodes)
-            {
-               if (trackNode != null && trackNode.UiD != null)
-               {
-                  var temp = GetFrontTraveller();
+         if (LeftButtonDown)
+         {
+            ShiftViewLeft();
+         }
 
-                  var v = temp.WorldLocation.Location.X;
-
-                  if (/*trackNode.UiD.WorldID == 552*/ true)
-                  {
-                     //g.FillEllipse(Brushes.Black, Dot(trackNode.UiD.WorldTileX * 2048 + trackNode.UiD.X, trackNode.UiD.TileZ * 2048 + trackNode.UiD.Z, 2));
-                     g.FillEllipse(Brushes.Black, Dot(trackNode.UiD.WorldTileX * scale + 1200, trackNode.UiD.WorldTileZ * scale, 2));
-
-                  }
-
-
-
-                  //PointF start = new PointF(trackNode.UiD.X, trackNode.UiD.Y);
-
-                  //if (trackNode.TrVectorNode != null)
-                  //{
-                  //   foreach (var section in trackNode.TrVectorNode.TrVectorSections)
-                  //   {
-                  //      PointF end = new PointF(section.X, section.Y);
-
-                  //      g.DrawLine(Pens.Black, start, end);
-                  //   }
-                  //}
-
-
-
-                  //foreach (var pin in trackNode.TrPins)
-                  //{
-                  //   if (pin != null)
-                  //   {
-                  //      UiD connected = simulator.TDB.TrackDB.TrackNodes[pin.Link].UiD;
-                  //      if (connected != null)
-                  //      {
-
-                  //         PointF end = new PointF(connected.X, connected.Y);
-
-                  //         g.DrawLine(Pens.Black, start, end);
-                  //      }
-                  //   }
-                  //}
-               }
-
-
-
-            }
-
-
-            //if (viewer.SceneryDrawer != null)
-            //{
-            //   foreach (WorldFile wf in viewer.SceneryDrawer.WorldFiles)
-            //   {
-            //      foreach (var item in wf.SceneryObjects)
-            //      {
-            //         if (item is StaticTrackShape)
-            //         {
-            //            StaticTrackShape trackShape = item as StaticTrackShape;
-
-            //            //simulator.TDB.
-
-
-            //            WorldPosition wp = trackShape.Location;
-
-            //            int tileX = wp.TileX;
-            //            int tileY = wp.TileZ;
-
-            //            if (tileX == -11064 && tileY == 14264)
-            //            {
-
-            //               double universeX = trackShape.Location.WorldLocation.Location.X + 1024;
-            //               double universeY = -trackShape.Location.WorldLocation.Location.Z + 1024;
-
-            //               //trackShape.Location.Location.
-
-
-            //               g.FillEllipse(Brushes.Black, Dot((float)universeX,(float)universeY, 5));
-            //            }
-            //         }
-            //         else if (item is StaticShape)
-            //         {
-
-            //         }
-            //      }
-            //   }
-            //}  
+         if (RightButtonDown)
+         {
+            ShiftViewRight();
          }
       }
 
       /// <summary>
-      /// Helper function used to generate a dot for debugging purposes.
+      /// Initialises the picturebox and the image it contains. 
       /// </summary>
-      /// <param name="X"></param>
-      /// <param name="Y"></param>
-      /// <param name="Size"></param>
-      /// <returns></returns>
-      private RectangleF Dot(float X, float Y, float Size)
+      private void InitImage()
       {
-         return new RectangleF(X, Y, Size, Size);
+         pictureBox1.Width = IM_Width;
+         pictureBox1.Height = IM_Height;
+
+         if (pictureBox1.Image != null)
+         {
+            pictureBox1.Image.Dispose();
+         }
+
+         pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+      }
+
+      /// <summary>
+      /// Regenerates the 2D view. At the moment, examines the track network
+      /// each time the view is drawn. Later, the traversal and drawing can be separated.
+      /// </summary>
+      private void GenerateView()
+      {
+
+         float minX = float.MaxValue;
+         float minY = float.MaxValue;
+
+         float maxX = float.MinValue;
+         float maxY = float.MinValue;
+
+         List<LineSegment> segments = new List<LineSegment>();
+         List<PointF> switches = new List<PointF>();
+
+         TrackNode[] nodes = simulator.TDB.TrackDB.TrackNodes;
+
+         for (int i = 0; i < nodes.Length; i++)
+         {
+            TrackNode currNode = nodes[i];
+
+            if (currNode != null)
+            {
+
+               if (currNode.TrVectorNode != null)
+               {
+
+                  if (currNode.TrVectorNode.TrVectorSections.Length > 1)
+                  {
+                     AddSegments(segments, currNode.TrVectorNode.TrVectorSections, ref minX, ref minY, ref maxX, ref maxY);
+                  }
+                  else
+                  {
+                     
+                  }
+               }
+               else if (currNode.TrJunctionNode != null)
+               {
+                  switches.Add(new PointF(currNode.UiD.TileX * 2048 + currNode.UiD.X, currNode.UiD.TileZ * 2048 + currNode.UiD.Z));
+               }
+               else
+               {
+                  // for now, do nothing here... we might do something here later
+               }
+            }
+         }
+    
+         using(Graphics g = Graphics.FromImage(pictureBox1.Image))
+         {
+
+            g.Clear(Color.White);
+
+            // this is the total size of the entire viewable route (xRange == width, yRange == height in metres)
+            float xRange = maxX - minX;
+            float yRange = maxY - minY;
+
+            float xScale = pictureBox1.Width / ViewWindow.Width;
+            float yScale = pictureBox1.Height/ ViewWindow.Height;
+
+            foreach (var line in segments)
+            {
+
+               PointF scaledA = new PointF((line.A.X - minX - ViewWindow.X) * xScale, (line.A.Y - minY - ViewWindow.Y) * yScale);
+               PointF scaledB = new PointF((line.B.X - minX - ViewWindow.X) * xScale, (line.B.Y - minY - ViewWindow.Y) * yScale);
+
+               g.DrawLine(Pens.Gray, scaledA, scaledB);
+            }
+
+            if (showSwitches.Checked)
+            {
+               foreach (PointF sw in switches)
+               {
+                  PointF scaledSw = new PointF((sw.X - minX - ViewWindow.X) * xScale, (sw.Y - minY - ViewWindow.Y) * yScale);
+
+                  g.FillEllipse(Brushes.Black, Dot(scaledSw, 5f));
+               }
+            }
+
+         }
+
+
+         pictureBox1.Invalidate();
+      }
+
+      /// <summary>
+      /// Generates a rectangle representing a dot being drawn.
+      /// </summary>
+      /// <param name="p">Center point of the dot, in pixels.</param>
+      /// <param name="size">Size of the dot's diameter, in pixels</param>
+      /// <returns></returns>
+      private RectangleF Dot(PointF p, float size)
+      {
+         return new RectangleF(p.X - size / 2f, p.Y - size / 2f, size, size);
+      }
+
+      /// <summary>
+      /// Generates line segments from an array of TrVectorSection. Also computes 
+      /// the bounds of the entire route being drawn.
+      /// </summary>
+      /// <param name="segments"></param>
+      /// <param name="items"></param>
+      /// <param name="minX"></param>
+      /// <param name="minY"></param>
+      /// <param name="maxX"></param>
+      /// <param name="maxY"></param>
+      private static void AddSegments(List<LineSegment> segments, TrVectorSection[] items,  ref float minX, ref float minY, ref float maxX, ref float maxY)
+      {
+         for (int i = 0; i < items.Length - 1; i++)
+         {
+            PointF A = new PointF(items[i].TileX * 2048 + items[i].X, items[i].TileZ * 2048 + items[i].Z);
+            PointF B = new PointF(items[i + 1].TileX * 2048 + items[i + 1].X, items[i + 1].TileZ * 2048 + items[i+1].Z);
+
+            CalcBounds(ref maxX, A.X, true);
+            CalcBounds(ref maxY, A.Y, true);
+            CalcBounds(ref maxX, B.X, true);
+            CalcBounds(ref maxY, B.Y, true);
+
+            CalcBounds(ref minX, A.X, false);
+            CalcBounds(ref minY, A.Y, false);
+            CalcBounds(ref minX, B.X, false);
+            CalcBounds(ref minY, B.Y, false);
+
+            segments.Add(new LineSegment(A, B));
+         }
+      }
+
+      /// <summary>
+      /// Given a value representing a limit, evaluate if the given value exceeds the current limit.
+      /// If so, expand the limit.
+      /// </summary>
+      /// <param name="limit">The current limit.</param>
+      /// <param name="value">The value to compare the limit to.</param>
+      /// <param name="gt">True when comparison is greater-than. False if less-than.</param>
+      private static void CalcBounds(ref float limit, float value, bool gt)
+      {
+         if (gt)
+         {
+            if (value > limit)
+            {
+               limit = value;
+            }
+         }
+         else
+         {
+            if (value < limit)
+            {
+               limit = value;
+            }
+         }
+      }
+
+
+      private float ScrollSpeedX
+      {
+         get
+         {
+            return ViewWindow.Width * 0.10f;
+         }
+      }
+
+      private float ScrollSpeedY
+      {
+         get
+         {
+            return ViewWindow.Width * 0.10f;
+         }
+      }
+
+      private void refreshButton_Click(object sender, EventArgs e)
+      {
+         GenerateView();
+      }
+
+      private void leftButton_Click(object sender, EventArgs e)
+      {
+         ShiftViewLeft();
+      }
+
+      private void rightButton_Click(object sender, EventArgs e)
+      {
+         ShiftViewRight();
+      }
+
+      private void upButton_Click(object sender, EventArgs e)
+      {
+         ShiftViewUp();
+      }
+
+      private void downButton_Click(object sender, EventArgs e)
+      {
+         ShiftViewDown();
+      }
+
+      private void ShiftViewUp()
+      {
+         ViewWindow.Offset(0, -ScrollSpeedY);
+
+         GenerateView();
+      }
+      private void ShiftViewDown()
+      {
+         ViewWindow.Offset(0, ScrollSpeedY);
+
+         GenerateView();
+      }
+
+      private void ShiftViewRight()
+      {
+         ViewWindow.Offset(ScrollSpeedX, 0);
+
+         GenerateView();
+      }
+
+
+      private void ShiftViewLeft()
+      {
+         ViewWindow.Offset(-ScrollSpeedX, 0);
+
+         GenerateView();
+      }
+
+
+
+
+      private void windowSizeUpDown_ValueChanged(object sender, EventArgs e)
+      {
+         // this is the center, before increasing the size
+         PointF center = new PointF(ViewWindow.X + ViewWindow.Width / 2f, ViewWindow.Y + ViewWindow.Height / 2f);
+
+
+         float newSize = (float)windowSizeUpDown.Value;
+
+         ViewWindow = new RectangleF(center.X - newSize / 2f, center.Y - newSize / 2f, newSize, newSize);
+
+
+         GenerateView();
+
+      }
+
+
+      private void leftButton_MouseDown(object sender, MouseEventArgs e)
+      {
+         LeftButtonDown = true;
+      }
+
+      private void leftButton_MouseLeave(object sender, EventArgs e)
+      {
+         LeftButtonDown = false;
+      }
+
+      private void leftButton_MouseUp(object sender, MouseEventArgs e)
+      {
+         LeftButtonDown = false;
+      }
+
+      private void rightButton_MouseUp(object sender, MouseEventArgs e)
+      {
+         RightButtonDown = false;
+      }
+
+      private void rightButton_MouseDown(object sender, MouseEventArgs e)
+      {
+         RightButtonDown = true;
+      }
+
+      private void rightButton_MouseLeave(object sender, EventArgs e)
+      {
+         RightButtonDown = false;
+      }
+
+      private void upButton_MouseUp(object sender, MouseEventArgs e)
+      {
+         UpButtonDown = false;
+      }
+
+      private void upButton_MouseDown(object sender, MouseEventArgs e)
+      {
+         UpButtonDown = true;
+      }
+
+      private void upButton_MouseLeave(object sender, EventArgs e)
+      {
+         UpButtonDown = false;
+      }
+
+      private void downButton_MouseUp(object sender, MouseEventArgs e)
+      {
+         DownButtonDown = false;
+      }
+
+      private void downButton_MouseDown(object sender, MouseEventArgs e)
+      {
+         DownButtonDown = true;
+      }
+
+      private void downButton_MouseLeave(object sender, EventArgs e)
+      {
+         DownButtonDown = false;
+      }
+
+      private void showSwitches_CheckedChanged(object sender, EventArgs e)
+      {
+         GenerateView();
+      }
+      
+
+
+
+   }
+
+
+   /// <summary>
+   /// Defines a geometric line segment.
+   /// </summary>
+   internal struct LineSegment
+   {
+      public PointF A;
+      public PointF B;
+
+      public LineSegment(PointF A, PointF B)
+      {
+         this.A = A;
+         this.B = B;
       }
    }
+
 }
