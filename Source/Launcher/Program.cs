7c879c1362c60dcc636e781bcf6d28a75978932a
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2011-01-28 15:52:05 +0000

    Updated launcher to select correct menu, with MenuWPF the current default. Bound the launcher to .NET 2.0, so any computer with .NET 2.0 (even without any SP) will be able to run the launcher and get a message saying they need 3.5 SP1.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@560 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/Launcher/Program.cs
+++ Source/Launcher/Program.cs
@@ -1,4 +1,9 @@
-﻿/* ORTS Launcher
+﻿// COPYRIGHT 2009, 2011 by the Open Rails project.
+// This code is provided to enable you to contribute improvements to the open rails program.  
+// Use of the code for any other purpose or distribution of the code to anyone else
+// is prohibited without specific written permission from admin@openrails.org.
+
+/* ORTS Launcher
  * 
  * This is the program that users execute start ORTS.  
  * Its purpose is to check for required dependancies 
@@ -8,86 +13,100 @@
  * so that it is gauranteed to run.  Ideally this should even
  * be a native program so it can check for .NET as well.
  */
-/// COPYRIGHT 2009 by the Open Rails project.
-/// This code is provided to enable you to contribute improvements to the open rails program.  
-/// Use of the code for any other purpose or distribution of the code to anyone else
-/// is prohibited without specific written permission from admin@openrails.org.
-/// 
-/// Contributors
-///     2009-12-06  Edward Keenan 
-
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 using Microsoft.Win32;
-using System.IO;
-using System.Diagnostics;
 
 
 namespace ORTS
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            if (!XNAIsInstalled())
-            {
-                MessageBox.Show("Could not find XNA 3.1\n\nPlease search www.microsoft.com for\nXNA 3.1 redistributable");
-                return;
-            }
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
 
-            foreach (string filename in new string[] {
-                        "ICSharpCode.SharpZipLib.dll",
-                        "irrKlang.NET2.0.dll",
-                        "reader.dll",
-                        "Menu.exe",
-                        "RunActivity.exe" })
-            {
-                if (!File.Exists(filename))
-                {
-                    MessageBox.Show("Could not find " + filename);
-                    return;
-                }
-            }
+			// Check for any missing components.
+			var path = Path.GetDirectoryName(Application.ExecutablePath);
+			var missing = new List<string>();
+			CheckNetFx(missing);
+			CheckXNA(missing);
+			CheckOR(missing, path);
+			if (missing.Count > 0)
+			{
+				MessageBox.Show(Application.ProductName + " requires the following:\n\n" + String.Join("\n", missing.ToArray()), Application.ProductName);
+				return;
+			}
 
-            string menuFolder = Environment.CurrentDirectory;
+			// Default menu, with override from Registry.
+			var menu = "MenuWPF.exe";
+			using (var RK = Registry.CurrentUser.OpenSubKey(@"Software\OpenRails\ORTS"))
+			{
+				var value = SafeReadKey(RK, "LauncherMenu", 0);
+				if (value == 1)
+					menu = "Menu.exe";
+				if (value == 2)
+					menu = "MenuWPF.exe";
+			}
+			Process.Start(Path.Combine(path, menu));
+		}
 
-            ProcessStartInfo objPSI = new ProcessStartInfo();
-            objPSI.FileName = menuFolder + @"\menu.exe";
-            objPSI.WindowStyle = ProcessWindowStyle.Normal; // or Hidden, Maximized or Normal
+		static void CheckNetFx(List<string> missing)
+		{
+			using (var RK = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5"))
+				if ((SafeReadKey(RK, "Install", 0) == 1) && (SafeReadKey(RK, "SP", 0) >= 1))
+					return;
+			missing.Add("Microsoft .NET Framework 3.5 SP1");
+		}
 
-            // TODO ADD A CHECK FOR .NET 3.5
+		static void CheckXNA(List<string> missing)
+		{
+			foreach (var key in new[] { @"SOFTWARE\Wow6432Node\Microsoft\XNA\Framework\v3.1", @"SOFTWARE\Microsoft\XNA\Framework\v3.1" })
+			{
+				using (var RK = Registry.LocalMachine.OpenSubKey(key))
+					if (SafeReadKey(RK, "Installed", 0) == 1)
+						return;
+			}
+			missing.Add("Microsoft XNA Framework 3.1");
+		}
 
-            Process.Start(objPSI);
-        }
+		static void CheckOR(List<string> missing, string path)
+		{
+			foreach (var file in new[] {
+				// Required libraries:
+				"ICSharpCode.SharpZipLib.dll",
+				"irrKlang.NET2.0.dll",
+				"PIEHidDotNet.dll",
+				"Reader.dll",
+				// Programs:
+				"Menu.exe",
+				"MenuWPF.exe",
+				"RunActivity.exe",
+			})
+			{
+				if (!File.Exists(Path.Combine(path, file)))
+					missing.Add("File '" + file + "'");
+			}
+		}
 
-        static bool XNAIsInstalled()
-        {
-            try
-            {
-                // Restore retained settings
-                RegistryKey RK = null;
-                RegistryKey RK32 = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\XNA\\Framework\\v3.1");
-                RegistryKey RK64 = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Wow6432Node\\Microsoft\\XNA\\Framework\\v3.1");
-                if (RK32 != null)
-                    RK = RK32;
-                else if (RK64 != null)
-                    RK = RK64;
-                int installed = (int)RK.GetValue("Installed", 0);
-                if (installed == 1)
-                    return true;
-            }
-            catch (System.Exception)
-            {
-            }
-            return false;
-        }
-
-    }
+		static int SafeReadKey(RegistryKey key, string name, int defaultValue)
+		{
+			try
+			{
+				return (int)key.GetValue(name, defaultValue);
+			}
+			catch
+			{
+				return defaultValue;
+			}
+		}
+	}
 }
