45dfa80a04e1af79ebbd68768df8f477350e906b
wacampbell <wacampbell@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2011-12-14 16:36:30 +0000

    Created a separate project for development of architectural concepts. A link is provided to a google docs document to accompany the project file.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@907 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Architecture/Messaging.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORArchitecture
+{
+    /// <summary>
+    /// A mechanism for communicaton among modules.
+    /// </summary>
+    interface iListener
+    {
+        public void RecieveMessage( Messaging source, EventMessage message);
+    }
+
+    interface iSender
+    {
+        public void SendMessage(Messaging source, Messaging destination, EventMessage message );
+        // for example signal code might send a message to a train driver
+        //              SendMessage( this, Train4.Controller, EventMessage( "Hold at siding 7 for train 16" ) )
+    }
+
+    class EventMessage
+    {
+        // TO DEFINE
+    }
+
+}
