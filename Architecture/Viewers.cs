0ff8d51a24790d74ca76e7da22e158cccb0bcb6a
wacampbell <wacampbell@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2011-12-14 18:09:03 +0000

    Added template for Viewer and its subclasses.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@908 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Architecture/Viewers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ORArchitecture
+{
+    /// <summary>
+    /// The base viewer can The base viewer class would have capability similar to a 
+    /// real world train watcher.  It can move around and view the VirtualWorld, 
+    /// but is assigned no control interfaces.  
+    /// </summary>
+    interface Viewer
+    {
+        Viewer(  );
+    }
+
+
+    /// <summary>
+    /// Adds capability for controlling a train, including handling keyboard and mouse
+    /// inputs and translating them to trainController commands, and graphics representations
+    /// such as cab view etc.
+    /// </summary>
+    interface TrainDriver : Viewer
+    {
+        TrainDriver( iControlTrain trainController);
+    }
+
+    /// <summary>
+    /// A extension of the concept.  For example a player could control the gantry crane
+    /// at a container loading port 
+    /// </summary>
+    interface CraneDriver : Viewer
+    {
+        CraneDriver(iControlCrane craneController);
+    }
+
+    interface EditorView : Viewer
+    {
+        EditorView(iControlTerrain terrainController, iControlScenery sceneryController);
+    }
+
+}
