1a8a4bfb9d8430b7e563c2950601f3ccffb3203b
jtang <jtang@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2013-04-17 16:40:15 +0000

    Trying to fix the mile marker bug: https://bugs.launchpad.net/or/+bug/1159393
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@1572 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/Viewer3D/Shapes.cs
+++ Source/RunActivity/Viewer3D/Shapes.cs
@@ -420,24 +420,24 @@ namespace ORTS
                         var tX = GetTextureCoordX(speed[j]); var tY = GetTextureCoordY(speed[j]);
 
                         //the left-bottom vertex
-                        Vector3 v = new Vector3(offset.X, offset.Y, 0);
+                        Vector3 v = new Vector3(offset.X, offset.Y, 0.01f);
                         M.Rotate2D(rotation, ref v.X, ref v.Z);
-                        v += start; Vertex v1 = new Vertex(v.X, v.Y, v.Z + 0.01f, 0, 0, -1, tX, tY);
+                        v += start; Vertex v1 = new Vertex(v.X, v.Y, v.Z, 0, 0, -1, tX, tY);
 
                         //the right-bottom vertex
-                        v.X = offset.X + size; v.Y = offset.Y; v.Z = 0;
+                        v.X = offset.X + size; v.Y = offset.Y; v.Z = 0.01f;
                         M.Rotate2D(rotation, ref v.X, ref v.Z);
-                        v += start; Vertex v2 = new Vertex(v.X, v.Y, v.Z + 0.01f, 0, 0, -1, tX + 0.25f, tY);
+                        v += start; Vertex v2 = new Vertex(v.X, v.Y, v.Z, 0, 0, -1, tX + 0.25f, tY);
 
                         //the right-top vertex
-                        v.X = offset.X + size; v.Y = offset.Y + size; v.Z = 0;
+                        v.X = offset.X + size; v.Y = offset.Y + size; v.Z = 0.01f;
                         M.Rotate2D(rotation, ref v.X, ref v.Z);
-                        v += start; Vertex v3 = new Vertex(v.X, v.Y, v.Z + 0.01f, 0, 0, -1, tX + 0.25f, tY - 0.25f);
+                        v += start; Vertex v3 = new Vertex(v.X, v.Y, v.Z, 0, 0, -1, tX + 0.25f, tY - 0.25f);
 
                         //the left-top vertex
-                        v.X = offset.X; v.Y = offset.Y + size; v.Z = 0;
+                        v.X = offset.X; v.Y = offset.Y + size; v.Z = 0.01f;
                         M.Rotate2D(rotation, ref v.X, ref v.Z);
-                        v += start; Vertex v4 = new Vertex(v.X, v.Y, v.Z + 0.01f, 0, 0, -1, tX, tY - 0.25f);
+                        v += start; Vertex v4 = new Vertex(v.X, v.Y, v.Z, 0, 0, -1, tX, tY - 0.25f);
 
                         //memory may not be enough
                         if (NumVertices > maxVertex - 4)
