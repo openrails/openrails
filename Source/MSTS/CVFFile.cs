ea0318900db36c2f66c446538c79ece8373257a9
pzgulyas <pzgulyas@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2013-10-19 07:17:04 +0000

    Bug #1074702: Temporary fix for the following warning flooding the log: Disassembled texture invalid for (...) fire.ace
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@1821 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/MSTS/CVFFile.cs
+++ Source/MSTS/CVFFile.cs
@@ -211,7 +211,7 @@ namespace MSTS
                 new STFReader.TokenProcessor("cabsignaldisplay", ()=>{ Add(new CVCSignal(stf, basepath)); }), 
                 new STFReader.TokenProcessor("digital", ()=>{ Add(new CVCDigital(stf, basepath)); }), 
                 new STFReader.TokenProcessor("combinedcontrol", ()=>{ Add(new CVCDiscrete(stf, basepath)); }),
-                new STFReader.TokenProcessor("firebox", ()=>{ Add(new CVCDiscrete(stf, basepath)); }), 
+                new STFReader.TokenProcessor("firebox", ()=>{ Add(new CVCGauge(stf, basepath)); }), 
                 new STFReader.TokenProcessor("digitalclock", ()=>{ Add(new CVCDigitalClock(stf, basepath)); })
             });
             //TODO Uncomment when parsed all type
