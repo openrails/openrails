openrails/openrails

commit 110cc3a7aed6406a40b0860cac118548fda48a03

Fix sound stuttering where large tile numbers were used in route, by normalizing all sound world coordinates to camera tile. Fix candidate for wrong rolloff calculation in case high number were set for deactivation distance.
git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@1849 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

Source/RunActivity/RollingStock/TrainCar.cs
@@ -63,8 +63,9 @@ namespace ORTS
         public readonly string WagFilePath;
 		public string RealWagFilePath; //we are substituting missing remote cars in MP, so need to remember this
 
-        // Some housekeeping
+        // sound related variables
         public bool IsPartOfActiveTrain = true;
+        public List<int> SoundSourceIDs = new List<int>();
 
         // some properties of this car
         public float LengthM = 40;       // derived classes must overwrite these defaults
@@ -217,6 +218,7 @@ namespace ORTS
 
                 _PrevSpeedMpS = _SpeedMpS;
             }
+            UpdateSoundPosition();
         }
 
         /// <summary>
@@ -730,6 +732,38 @@ namespace ORTS
             if (maxV > 1 || Math.Abs(tz) > 0.02 || Math.Abs(AccelerationMpSS)>0.5) { accumedAcceTime = 1f; return max; }
             accumedAcceTime += interval/5;
             return max / accumedAcceTime;//otherwise slowly decrease the value
+        }
+
+        public float[] Velocity = new float[] { 0, 0, 0 };
+        WorldLocation SoundLocation;
+        public void UpdateSoundPosition()
+        {
+            if (SoundSourceIDs.Count == 0 || Program.Simulator.Confirmer.Viewer == null || Program.Simulator.Confirmer.Viewer.Camera == null)
+                return;
+            
+            if (Train != null)
+            {
+                Vector3 directionVector = Vector3.Multiply(GetXNAMatrix().Forward, SpeedMpS);
+                Velocity = new float[] { directionVector.X, directionVector.Y, -directionVector.Z };
+            }
+            else
+                Velocity = new float[] { 0, 0, 0 };
+
+            SoundLocation = WorldPosition.WorldLocation;
+            SoundLocation.NormalizeTo(
+                Program.Simulator.Confirmer.Viewer.Camera.CameraWorldLocation.TileX,
+                Program.Simulator.Confirmer.Viewer.Camera.CameraWorldLocation.TileZ);
+            float[] position = new float[] {
+                SoundLocation.Location.X,
+                SoundLocation.Location.Y,
+                SoundLocation.Location.Z};
+
+            var soundSourceIDs = SoundSourceIDs.ToArray();
+            foreach (var soundSourceID in soundSourceIDs)
+            {
+                OpenAL.alSourcefv(soundSourceID, OpenAL.AL_POSITION, position);
+                OpenAL.alSourcefv(soundSourceID, OpenAL.AL_VELOCITY, Velocity);
+            }
         }
     }
 

Source/RunActivity/Viewer3D/Sound.cs
@@ -41,7 +41,6 @@
 #define PLAYENVSOUNDS
 //#define DEBUGSCR
 #define STEREOCAB
-#define DOPPLER
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -206,6 +205,11 @@ namespace ORTS
     public class SoundSource : SoundSourceBase
     {
         private const int CUTOFFDISTANCE = 100000;
+        public const float MaxDistanceM = 316f; // (float)Math.Sqrt(CUTOFFDISTANCE)
+        public const float GainAtMaxDistance = 0.01f;
+        public const float ReferenceDistanceM = 5f; // below is no attenuation
+        public float RolloffFactor;
+
         /// <summary>
         /// Construct a SoundSource attached to a train car.
         /// </summary>
@@ -246,11 +250,11 @@ namespace ORTS
         /// <param name="viewer"></param>
         /// <param name="worldLocation"></param>
         /// <param name="smsFilePath"></param>
-        /// <param name="isSlowRolloff"></param>
-        public SoundSource(Viewer3D viewer, WorldLocation worldLocation, Events.Source eventSource, string smsFilePath, bool isSlowRolloff)
+        /// <param name="slowRolloff"></param>
+        public SoundSource(Viewer3D viewer, WorldLocation worldLocation, Events.Source eventSource, string smsFilePath, bool slowRolloff)
         {
             IsEnvSound = true;
-            _isSlowRolloff = isSlowRolloff;
+            SlowRolloff = slowRolloff;
             Initialize(viewer, worldLocation, eventSource, smsFilePath);
         }
 
@@ -280,7 +284,7 @@ namespace ORTS
 
         public float DistanceSquared = CUTOFFDISTANCE + 1;
         private bool WasOutOfDistance = true;
-        private bool _isSlowRolloff;
+        private bool SlowRolloff;
 
         public List<SoundStream> SoundStreams = new List<SoundStream>();
 
@@ -310,11 +314,16 @@ namespace ORTS
                 Volume = mstsScalabiltyGroup.Volume;
                 Ignore3D = mstsScalabiltyGroup.Ignore3D | mstsScalabiltyGroup.Stereo;
                 IsExternal = ActivationConditions.ExternalCam;
+
+                var maxDistanceM = DeactivationConditions.Distance == 0 ? MaxDistanceM : Math.Min(MaxDistanceM, DeactivationConditions.Distance);
+
+                // OpenAL distance model is based on formula
+                // Gain = AL_REFERENCE_DISTANCE / ( AL_REFERENCE_DISTANCE + AL_ROLLOFF_FACTOR * ( Distance - AL_REFERENCE_DISTANCE ) )
+                RolloffFactor = SlowRolloff ? 0.4f : ReferenceDistanceM * (1f / GainAtMaxDistance - 1f) / (maxDistanceM - ReferenceDistanceM);
                 
-                int cou = 1;
                 foreach (MSTS.SMSStream mstsStream in mstsScalabiltyGroup.Streams)
                 {
-                    SoundStreams.Add(new SoundStream(mstsStream, eventSource, this, cou++, _isSlowRolloff, ActivationConditions.Distance));
+                    SoundStreams.Add(new SoundStream(mstsStream, eventSource, this));
                 }
             }
         }
@@ -343,6 +352,9 @@ namespace ORTS
             {
                 if (!WasOutOfDistance)
                 {
+                    if (Car != null)
+                        Car.SoundSourceIDs.Clear();
+
                     foreach (SoundStream stream in SoundStreams)
                         stream.HardDeactivate();
                 }
@@ -376,6 +388,9 @@ namespace ORTS
                             }
                         }
                         stream.ALSoundSource.Set2D(WorldLocation == null || Ignore3D || !IsExternal);
+
+                        if (Car != null)
+                            Car.SoundSourceIDs.Add(stream.ALSoundSource.SoundSourceID);
                     }
                 }
                 WasOutOfDistance = false;
@@ -416,38 +431,17 @@ namespace ORTS
 
             bool needsFrequentUpdate = false;
 
-            // Must start and stop by triggers - by GeorgeS
-            //if (Active)
-            if (WorldLocation != null && !Ignore3D && IsExternal)
+            if (Car == null && WorldLocation != null && !Ignore3D && IsExternal)
             {
-                float[] velocity = new float[] {0, 0, 0};
-
-#if DOPPLER
-                // Stationary or otherwise invalid Car
-                if (Car != null && Car.Train != null)
-                {
-                    if (Viewer.Camera.AttachedCar != null && Viewer.Camera.AttachedCar.Train != null 
-                        && Car.Train == Viewer.Camera.AttachedCar.Train
-                        && !(Viewer.Camera is TracksideCamera) && !(Viewer.Camera is FreeRoamCamera))
-                    {
-                        Vector3 directionVector = Vector3.Multiply(Viewer.Camera.AttachedCar.GetXNAMatrix().Forward, Viewer.Camera.AttachedCar.SpeedMpS);
-                        velocity = new float[] { directionVector.X, directionVector.Y, -directionVector.Z };
-                    }
-                    else
-                    {
-                        Vector3 directionVector = Vector3.Multiply(Car.GetXNAMatrix().Forward, Car.SpeedMpS);
-                        velocity = new float[] { directionVector.X, directionVector.Y, -directionVector.Z };
-                    }
-                }
-#endif
+                WorldLocation.NormalizeTo(Viewer.Camera.CameraWorldLocation.TileX, Viewer.Camera.CameraWorldLocation.TileZ);
                 float[] position = new float[] {
-                    WorldLocation.Location.X + 2048 * WorldLocation.TileX,
+                    WorldLocation.Location.X,
                     WorldLocation.Location.Y,
-                    WorldLocation.Location.Z + 2048 * WorldLocation.TileZ};
+                    WorldLocation.Location.Z};
 
                 foreach (SoundStream stream in SoundStreams)
                 {
-                    stream.Update(position, velocity);
+                    stream.Update(position);
                     needsFrequentUpdate |= stream.NeedsFrequentUpdate;
                 }
             }
@@ -604,7 +598,6 @@ namespace ORTS
     public class SoundStream : IDisposable
     {
         public SoundSource SoundSource;
-        public int Index;
         
         public float Volume;
 
@@ -625,14 +618,13 @@ namespace ORTS
         List<ORTSTrigger> VariableTriggers;
         IEnumerable<ORTSTrigger> TriggersList;
 
-        public SoundStream(MSTS.SMSStream mstsStream, Events.Source eventSource, SoundSource soundSource, int index, bool isSlowRolloff, float factor)
+        public SoundStream(MSTS.SMSStream mstsStream, Events.Source eventSource, SoundSource soundSource)
         {
-            Index = index;
             SoundSource = soundSource;
             MSTSStream = mstsStream;
             Volume = MSTSStream.Volume;
 
-            ALSoundSource = new ALSoundSource(soundSource.IsEnvSound, isSlowRolloff, factor);
+            ALSoundSource = new ALSoundSource(soundSource.IsEnvSound, soundSource.RolloffFactor);
 
             if (mstsStream.Triggers != null) 
                 foreach( MSTS.Trigger trigger in mstsStream.Triggers )
@@ -678,10 +670,9 @@ namespace ORTS
                                 select t).ToList();
         }
 
-        public void Update(float[] position, float[] velocity)
+        public void Update(float[] position)
         {
-            ALSoundSource.SetPosition(position);
-            ALSoundSource.SetVelocity(velocity);
+            OpenAL.alSourcefv(ALSoundSource.SoundSourceID, OpenAL.AL_POSITION, position);
             Update();
         }
 

Source/RunActivity/MultiPlayer/Message.cs
@@ -510,7 +510,6 @@ namespace ORTS.MultiPlayer
 					Program.Simulator.Weather = (WeatherType)this.weather;
 					Program.Simulator.ClockTime = this.seconds;
 					Program.Simulator.Season = (SeasonType)this.season;
-                    Program.Simulator.Confirmer.Viewer.World.WeatherControl.SetWeatherParams();
 				}
 			}
 		}

Source/RunActivity/Processes/SoundProcess.cs
@@ -17,8 +17,6 @@
 
 // This file is the responsibility of the 3D & Environment Team. 
 
-#define DOPPLER
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -173,32 +171,7 @@ namespace ORTS
                     if (Viewer.World.GameSounds != null) Viewer.World.GameSounds.HandleEvent(Viewer.Simulator.SoundNotify);
                     Viewer.Simulator.SoundNotify = Event.None;
 				}
-#if DOPPLER
-                if (Viewer != null || Viewer.Camera != null) // For sure we have a Camera
-                {
-                    float[] cameraPosition = new float[] {
-                        Viewer.Camera.CameraWorldLocation.Location.X + 2048 * Viewer.Camera.CameraWorldLocation.TileX,
-                        Viewer.Camera.CameraWorldLocation.Location.Y,
-                        Viewer.Camera.CameraWorldLocation.Location.Z + 2048 * Viewer.Camera.CameraWorldLocation.TileZ};
 
-                    float[] cameraVelocity = new float[] { 0, 0, 0 };
-
-                    if (!(Viewer.Camera is TracksideCamera) && !(Viewer.Camera is FreeRoamCamera) && Viewer.Camera.AttachedCar != null)
-                    {
-                        Vector3 directionVector = Vector3.Multiply(Viewer.Camera.AttachedCar.GetXNAMatrix().Forward, Viewer.Camera.AttachedCar.SpeedMpS);
-                        cameraVelocity = new float[] { directionVector.X, directionVector.Y, -directionVector.Z };
-                    }
-
-                    float[] cameraOrientation = new float[] { 
-                        Viewer.Camera.XNAView.Backward.X, Viewer.Camera.XNAView.Backward.Y, Viewer.Camera.XNAView.Backward.Z,
-                        Viewer.Camera.XNAView.Down.X, Viewer.Camera.XNAView.Down.Y, Viewer.Camera.XNAView.Down.Z };
-
-                    OpenAL.alListenerfv(OpenAL.AL_POSITION, cameraPosition);
-                    OpenAL.alListenerfv(OpenAL.AL_VELOCITY, cameraVelocity);
-                    OpenAL.alListenerfv(OpenAL.AL_ORIENTATION, cameraOrientation);
-                    OpenAL.alListenerf(OpenAL.AL_GAIN, Program.Simulator.Paused ? 0 : (float)Program.Simulator.Settings.SoundVolumePercent / 100f);
-                }
-#endif
                 // Update all sound in our list
 				lock (SoundSources)
 				{

Source/RunActivity/Viewer3D/ALSoundHelper.cs
@@ -456,9 +456,8 @@ namespace ORTS
         private const int QUEUELENGHT = 16;
 
         public int SoundSourceID = -1;
-        bool _isSlowRolloff;
-        bool _isLooping;
-        float _distanceFactor = 10;
+        bool Looping;
+        float RolloffFactor = 1;
 
         private SoundItem[] SoundQueue = new SoundItem[QUEUELENGHT];
         private int QueueHeader;
@@ -470,14 +469,12 @@ namespace ORTS
         /// Constructs a new AL sound source
         /// </summary>
         /// <param name="isEnv">True if environment sound</param>
-        /// <param name="isSlowRolloff">True if stationary, slow roll off sound</param>
-        /// <param name="distanceFactor">The number indicating the fade speed by the distance</param>
-        public ALSoundSource(bool isEnv, bool isSlowRolloff, float distanceFactor)
+        /// <param name="rolloffFactor">The number indicating the fade speed by the distance</param>
+        public ALSoundSource(bool isEnv, float rolloffFactor)
         {
             SoundSourceID = -1;
             SoundQueue[QueueTail].PlayState = PlayState.NOP;
-            _isSlowRolloff = isSlowRolloff;
-            _distanceFactor = 1 / (distanceFactor / 350);
+            RolloffFactor = rolloffFactor;
         }
 
         private bool _nxtUpdate;
@@ -499,17 +496,10 @@ namespace ORTS
                         _MustActivate = false;
                         _MustWarn = true;
 
-                        //OpenAL.alSourcef(SoundSourceID, OpenAL.AL_MAX_DISTANCE, _distanceFactor);
-                        OpenAL.alSourcef(SoundSourceID, OpenAL.AL_REFERENCE_DISTANCE, 5f); // meter - below is no attenuation
-                        OpenAL.alSourcef(SoundSourceID, OpenAL.AL_MAX_GAIN, 1);
-                        if (_isSlowRolloff)
-                        {
-                            OpenAL.alSourcef(SoundSourceID, OpenAL.AL_ROLLOFF_FACTOR, .4f);
-                        }
-                        else
-                        {
-                            OpenAL.alSourcef(SoundSourceID, OpenAL.AL_ROLLOFF_FACTOR, _distanceFactor);
-                        }
+                        OpenAL.alSourcef(SoundSourceID, OpenAL.AL_MAX_DISTANCE, SoundSource.MaxDistanceM);
+                        OpenAL.alSourcef(SoundSourceID, OpenAL.AL_REFERENCE_DISTANCE, SoundSource.ReferenceDistanceM);
+                        OpenAL.alSourcef(SoundSourceID, OpenAL.AL_MAX_GAIN, 1f);
+                        OpenAL.alSourcef(SoundSourceID, OpenAL.AL_ROLLOFF_FACTOR, RolloffFactor);
 
                         if (_Active)
                             SetVolume(_Volume);
@@ -517,7 +507,7 @@ namespace ORTS
                             SetVolume(0);
 
                         OpenAL.alSourcef(SoundSourceID, OpenAL.AL_PITCH, _PlaybackSpeed);
-                        OpenAL.alSourcei(SoundSourceID, OpenAL.AL_LOOPING, _isLooping ? OpenAL.AL_TRUE : OpenAL.AL_FALSE);
+                        OpenAL.alSourcei(SoundSourceID, OpenAL.AL_LOOPING, Looping ? OpenAL.AL_TRUE : OpenAL.AL_FALSE);
                     }
                     else if (_MustWarn)
                     {
@@ -531,16 +521,6 @@ namespace ORTS
         private void SetVolume(float volume)
         {
             OpenAL.alSourcef(SoundSourceID, OpenAL.AL_GAIN, volume);
-        }
-
-        public void SetPosition(float[] position)
-        {
-            OpenAL.alSourcefv(SoundSourceID, OpenAL.AL_POSITION, position);
-        }
-
-        public void SetVelocity(float[] velocity)
-        {
-            OpenAL.alSourcefv(SoundSourceID, OpenAL.AL_VELOCITY, velocity);
         }
 
         public void Set2D(bool sound2D)
@@ -1016,17 +996,17 @@ namespace ORTS
 
         private void EnterLoop()
         {
-            if (_isLooping)
+            if (Looping)
                 return;
 
             OpenAL.alSourcei(SoundSourceID, OpenAL.AL_LOOPING, OpenAL.AL_TRUE);
-            _isLooping = true;
+            Looping = true;
         }
 
         private void LeaveLoop()
         {
             OpenAL.alSourcei(SoundSourceID, OpenAL.AL_LOOPING, OpenAL.AL_FALSE);
-            _isLooping = false;
+            Looping = false;
         }
 
         private static bool _Muted;

Source/RunActivity/Viewer3D/Cameras.cs
@@ -277,6 +277,29 @@ namespace ORTS
                 return MathHelper.Clamp(angle, Minimum, Maximum);
             }
         }
+
+        public void UpdateListener()
+        {
+            float[] cameraPosition = new float[] {
+                        CameraWorldLocation.Location.X,
+                        CameraWorldLocation.Location.Y,
+                        CameraWorldLocation.Location.Z};
+
+            float[] cameraVelocity = new float[] { 0, 0, 0 };
+
+            if (!(this is TracksideCamera) && !(this is FreeRoamCamera) && AttachedCar != null)
+            {
+                cameraVelocity = AttachedCar.Velocity;
+            }
+
+            float[] cameraOrientation = new float[] { 
+                        XNAView.Backward.X, XNAView.Backward.Y, XNAView.Backward.Z,
+                        XNAView.Down.X, XNAView.Down.Y, XNAView.Down.Z };
+
+            OpenAL.alListenerfv(OpenAL.AL_POSITION, cameraPosition);
+            OpenAL.alListenerfv(OpenAL.AL_VELOCITY, cameraVelocity);
+            OpenAL.alListenerfv(OpenAL.AL_ORIENTATION, cameraOrientation);
+        }
     }
 
     public abstract class LookAtCamera : Camera
@@ -684,6 +707,7 @@ namespace ORTS
                     }
                 }
             }
+            UpdateListener();
         }
 
         protected virtual void PanRight(float speed)
@@ -905,6 +929,7 @@ namespace ORTS
                 cameraLocation.Location.Z *= -1;
             }
             UpdateRotation(elapsedTime);
+            UpdateListener();
         }
     }
 
@@ -1118,6 +1143,7 @@ namespace ORTS
 
             // Update location of camera
             UpdateLocation();
+            UpdateListener();
         }
 
         protected void PanUp(float speed)
@@ -1619,6 +1645,7 @@ namespace ORTS
             }
 
             targetLocation.Location.Y += TargetAltitude;
+            UpdateListener();
         }
     }
 }

commit 626b6044ce83f41130a2e83bc93717abf0242bc8

Fix sound source positioning issue introduced in r1849
git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@1861 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

Source/RunActivity/RollingStock/TrainCar.cs
@@ -736,6 +736,7 @@ namespace ORTS
 
         public float[] Velocity = new float[] { 0, 0, 0 };
         WorldLocation SoundLocation;
+        static WorldLocation CameraWorldLocation;
         public void UpdateSoundPosition()
         {
             if (SoundSourceIDs.Count == 0 || Program.Simulator.Confirmer.Viewer == null || Program.Simulator.Confirmer.Viewer.Camera == null)
@@ -750,9 +751,8 @@ namespace ORTS
                 Velocity = new float[] { 0, 0, 0 };
 
             SoundLocation = WorldPosition.WorldLocation;
-            SoundLocation.NormalizeTo(
-                Program.Simulator.Confirmer.Viewer.Camera.CameraWorldLocation.TileX,
-                Program.Simulator.Confirmer.Viewer.Camera.CameraWorldLocation.TileZ);
+            CameraWorldLocation = Program.Simulator.Confirmer.Viewer.Camera.CameraWorldLocation;
+            SoundLocation.NormalizeTo(CameraWorldLocation.TileX, CameraWorldLocation.TileZ);
             float[] position = new float[] {
                 SoundLocation.Location.X,
                 SoundLocation.Location.Y,

Source/RunActivity/Viewer3D/Sound.cs
@@ -298,6 +298,8 @@ namespace ORTS
         {
             foreach (SoundStream ss in SoundStreams)
             {
+                if (Car != null)
+                    Car.SoundSourceIDs.Remove(ss.ALSoundSource.SoundSourceID);
                 ss.Stop();
                 ss.HardDeactivate();
                 WasOutOfDistance = true;
@@ -465,6 +467,7 @@ namespace ORTS
             }
         }
         
+        static WorldLocation CameraWorldLocation;
         public override bool Update()
         {
             if (Car != null && !Car.IsPartOfActiveTrain)
@@ -501,7 +504,8 @@ namespace ORTS
 
             if (Car == null && WorldLocation != null && !Ignore3D && IsExternal)
             {
-                WorldLocation.NormalizeTo(Viewer.Camera.CameraWorldLocation.TileX, Viewer.Camera.CameraWorldLocation.TileZ);
+                CameraWorldLocation = Viewer.Camera.CameraWorldLocation;
+                WorldLocation.NormalizeTo(CameraWorldLocation.TileX, CameraWorldLocation.TileZ);
                 float[] position = new float[] {
                     WorldLocation.Location.X,
                     WorldLocation.Location.Y,