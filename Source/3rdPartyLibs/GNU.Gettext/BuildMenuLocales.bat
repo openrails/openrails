459cbc19027e4fea11a066f4e7489e8dfccb0aa1
pzgulyas <pzgulyas@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2014-02-13 22:04:40 +0000

    Modify building method of locales
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@2026 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

---
+++ Source/3rdPartyLibs/GNU.Gettext/BuildMenuLocales.bat
@@ -0,0 +1,3 @@
+rem Script starts in \OpenRails\Program\
+..\Source\3rdPartyLibs\GNU.Gettext\GNU.Gettext.Msgfmt.exe -l fr -r ORTS.Menu -d .\ -L GNU.Gettext.dll ..\Source\3rdPartyLibs\GNU.Gettext\Locales\Menu\fr.po
+..\Source\3rdPartyLibs\GNU.Gettext\GNU.Gettext.Msgfmt.exe -l hu -r ORTS.Menu -d .\ -L GNU.Gettext.dll ..\Source\3rdPartyLibs\GNU.Gettext\Locales\Menu\hu.po
--- Source/3rdPartyLibs/GNU.Gettext/Pre-build.bat
+++
@@ -1,4 +0,0 @@
-rem GNU.Gettext.Msgfmt.exe -l fr -r ORTS.Menu -d ..\..\..\Program -L GNU.Gettext.dll Locales\Menu\fr.po
-rem GNU.Gettext.Msgfmt.exe -l hu -r ORTS.Menu -d ..\..\..\Program -L GNU.Gettext.dll Locales\Menu\hu.po
-GNU.Gettext.Msgfmt.exe -l fr -r ORTS.Menu -d ..\..\Menu -L GNU.Gettext.dll Locales\Menu\fr.po
-GNU.Gettext.Msgfmt.exe -l hu -r ORTS.Menu -d ..\..\Menu -L GNU.Gettext.dll Locales\Menu\hu.po
