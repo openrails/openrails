b4b4843d6ee6561db943146049020da501ec1bb5
carlossantucci <carlossantucci@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2014-05-24 15:48:59 +0000

    Fix for bug #1322823 Ctrl+Alt+F6 (Tracks) crash OR in small routes
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@2253 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/Viewer3D/Popups/TracksDebugWindow.cs
+++ Source/RunActivity/Viewer3D/Popups/TracksDebugWindow.cs
@@ -90,25 +90,28 @@ namespace ORTS.Viewer3D.Popups
                         }
                     }
                 }
-                foreach (var trackNode in rdb.RoadTrackDB.TrackNodes.Where(tn => tn != null && tn.TrVectorNode != null && Math.Abs(tn.TrVectorNode.TrVectorSections[0].TileX - camera.TileX) <= 1 && Math.Abs(tn.TrVectorNode.TrVectorSections[0].TileZ - camera.TileZ) <= 1))
+                if (rdb != null)
                 {
-                    var currentPosition = new Traveller(tSectionDat, rdb.RoadTrackDB.TrackNodes, trackNode);
-                    while (true)
+                    foreach (var trackNode in rdb.RoadTrackDB.TrackNodes.Where(tn => tn != null && tn.TrVectorNode != null && Math.Abs(tn.TrVectorNode.TrVectorSections[0].TileX - camera.TileX) <= 1 && Math.Abs(tn.TrVectorNode.TrVectorSections[0].TileZ - camera.TileZ) <= 1))
                     {
-                        var previousLocation = currentPosition.WorldLocation;
-                        var remaining = currentPosition.MoveInSection(DisplaySegmentLength);
-                        if ((Math.Abs(remaining - DisplaySegmentLength) < Tolerance) && !currentPosition.NextVectorSection())
-                            break;
-                        primitives.Add(new DispatcherLineSegment(previousLocation, currentPosition.WorldLocation, Color.LightSalmon, 2));
-                    }
-                    if (trackNode.TrVectorNode.TrItemRefs != null)
-                    {
-                        foreach (var trItemID in trackNode.TrVectorNode.TrItemRefs)
+                        var currentPosition = new Traveller(tSectionDat, rdb.RoadTrackDB.TrackNodes, trackNode);
+                        while (true)
                         {
-                            var trItem = rdb.RoadTrackDB.TrItemTable[trItemID];
-                            currentPosition = new Traveller(tSectionDat, rdb.RoadTrackDB.TrackNodes, trackNode);
-                            currentPosition.Move(trItem.SData1);
-                            primitives.Add(new DispatcherLabel(currentPosition.WorldLocation, Color.LightSalmon, String.Format("{0} {1} {2}", trItem.TrItemId, trItem.ItemType.ToString().Replace("tr", "").ToUpperInvariant(), trItem.ItemName), Owner.TextFontDefaultOutlined));
+                            var previousLocation = currentPosition.WorldLocation;
+                            var remaining = currentPosition.MoveInSection(DisplaySegmentLength);
+                            if ((Math.Abs(remaining - DisplaySegmentLength) < Tolerance) && !currentPosition.NextVectorSection())
+                                break;
+                            primitives.Add(new DispatcherLineSegment(previousLocation, currentPosition.WorldLocation, Color.LightSalmon, 2));
+                        }
+                        if (trackNode.TrVectorNode.TrItemRefs != null)
+                        {
+                            foreach (var trItemID in trackNode.TrVectorNode.TrItemRefs)
+                            {
+                                var trItem = rdb.RoadTrackDB.TrItemTable[trItemID];
+                                currentPosition = new Traveller(tSectionDat, rdb.RoadTrackDB.TrackNodes, trackNode);
+                                currentPosition.Move(trItem.SData1);
+                                primitives.Add(new DispatcherLabel(currentPosition.WorldLocation, Color.LightSalmon, String.Format("{0} {1} {2}", trItem.TrItemId, trItem.ItemType.ToString().Replace("tr", "").ToUpperInvariant(), trItem.ItemName), Owner.TextFontDefaultOutlined));
+                            }
                         }
                     }
                 }
