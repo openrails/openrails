8dac505c1dcf68008cf057840a8f9aa811aeb6fb
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2014-08-10 10:25:51 +0000

    Fix for website user tracking code balking if two new users access it on the same second.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@2404 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Website/openrails.org/shared/mysql/track_visits.php
+++ Website/openrails.org/shared/mysql/track_visits.php
@@ -8,30 +8,24 @@ $path = strtr($root, '\\', '/');
 // Retrieve id from cookie or create a new one.
 $cookie_name = 'or_org3';
 if(isset($_COOKIE[$cookie_name])) {
-  $id = $_COOKIE[$cookie_name];
-  #echo "old id = $id<br>";
+	$id = $_COOKIE[$cookie_name];
 }else{	
-	$id = md5(date("Y-m-d H:i:s")); // Uses the MySQL DATETIME format
-  setcookie($cookie_name, $id, 2147483647); // max time value to expire far into future
-  #echo "new id = $id<br>";
-  $sql = "INSERT INTO tVisitor (id) VALUES('$id')";
-  #echo "$sql <br>";
-  if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
-	
+	$id = md5(date("Y-m-d H:i:s") + rand()); // Uses the MySQL DATETIME format plus random number
+	setcookie($cookie_name, $id, 2147483647); // max time value to expire far into future (19-Jan-2038)
+	$sql = "INSERT INTO tVisitor (id) VALUES('$id')";
+	if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
+
 	$ip = $_SERVER['REMOTE_ADDR']; // Always a real IP address but might be a proxy.
-  $sql = "INSERT INTO tVisitor_Attribute (used_by, of_type, of_value) VALUES('$id', 'ip', '$ip')";
-  #echo "$sql <br>";
-  if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
-	
+	$sql = "INSERT INTO tVisitor_Attribute (used_by, of_type, of_value) VALUES('$id', 'ip', '$ip')";
+	if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
+
 	$referer = $_SERVER['HTTP_REFERER']; // Always a real IP address but might be a proxy.
-  $sql = "INSERT INTO tVisitor_Attribute (used_by, of_type, of_value) VALUES('$id', 'referer', '$referer')";
-  #echo "$sql <br>";
-  if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
+	$sql = "INSERT INTO tVisitor_Attribute (used_by, of_type, of_value) VALUES('$id', 'referer', '$referer')";
+	if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
 	
 	$agent = $_SERVER['HTTP_USER_AGENT'];
-  $sql = "INSERT INTO tVisitor_Attribute (used_by, of_type, of_value) VALUES('$id', 'agent', '$agent')";
-  #echo "$sql <br>";
-  if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
+	$sql = "INSERT INTO tVisitor_Attribute (used_by, of_type, of_value) VALUES('$id', 'agent', '$agent')";
+	if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
 }
 
 // Record visit made to this webpage
@@ -41,6 +35,5 @@ $made_to = str_replace('//', '/', $made_to); // home page is just '/', but other
 
 // In a timestamp field, NULL is replaced by current time automatically.
 $sql = "INSERT INTO tVisit (made_by, made_to, visited_on) VALUES('$id', '$made_to', NULL)"; 
-#echo "$sql <br>";
 if (!mysqli_query($dbc, $sql)) { die('Error: ' . mysqli_error($dbc)); }
 ?>
