cdbd94edc2b0d148a36ad567fd65d691f7ac53ce
steamer_ctn <steamer_ctn@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2014-12-16 06:24:01 +0000

    CylinderEfficiency uncapped
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@2720 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/RollingStock/MSTSSteamLocomotive.cs
+++ Source/RunActivity/RollingStock/MSTSSteamLocomotive.cs
@@ -849,10 +849,10 @@ namespace ORTS
 
           if (CylinderEfficiencyRate == 0)
           {
-              CylinderEfficiencyRate = 1.0f; // If no cyldinder efficiency rate in the ENG file set to mormal (1.0)
+              CylinderEfficiencyRate = 1.0f; // If no cylinder efficiency rate in the ENG file set to mormal (1.0)
           }
 
-          CylinderEfficiencyRate = MathHelper.Clamp(CylinderEfficiencyRate, 0.6f, 1.2f); // Clamp Cylinder Efficiency Rate to between 0.6 & 1.2           
+        //  CylinderEfficiencyRate = MathHelper.Clamp(CylinderEfficiencyRate, 0.6f, 1.2f); // Clamp Cylinder Efficiency Rate to between 0.6 & 1.2           
           MaxTractiveEffortLbf = (NumCylinders / 2.0f) * (Me.ToIn(CylinderDiameterM) * Me.ToIn(CylinderDiameterM) * Me.ToIn(CylinderStrokeM) / (2 * Me.ToIn(DriverWheelRadiusM))) * MaxBoilerPressurePSI * TractiveEffortFactor * MotiveForceGearRatio * CylinderEfficiencyRate;
 
             // Max IHP = (Max TE x Speed) / 375.0, use a factor of 0.85 to calculate max TE
