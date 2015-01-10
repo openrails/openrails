8baa707645bfd0edf2c0fd833871ad7cc13113a8
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2015-01-10 17:53:20 +0000

    Make sure the menu's consist drop-down is cleared when changing from an activity to explore mode if the locomotive data hasn't loaded yet.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@2782 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/Menu/MainForm.cs
+++ Source/Menu/MainForm.cs
@@ -376,6 +376,7 @@ namespace ORTS
         {
             comboBoxTimetable.SelectedItem = null;
             ShowLocomotiveList();
+            ShowConsistList();
             ShowStartAtList();
             ShowEnvironment();
             ShowDetails();
