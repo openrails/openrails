1e1d9de447a2ae3f6dfe88c299d53c5ed036eaf1
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2015-04-16 21:22:46 +0000

    http://www.elvastower.com/forums/index.php?/topic/25747-manual-omission/page__view__findpost__p__178198 Disable the Activity Editor contributed tool as it's not ready for use by anyone yet.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3007 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/Contrib/ActivityEditor/ActivityEditor/Properties/AssemblyInfo.cs
+++ Source/Contrib/ActivityEditor/ActivityEditor/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Open Rails Transport Simulator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Open Rails")]
-[assembly: AssemblyProduct("Open Rails")]
+[assembly: AssemblyProduct("Open Rails (disabled)")]
 [assembly: AssemblyCopyright("Copyright © 2009, 2010, 2011, 2012, 2013, 2014, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
