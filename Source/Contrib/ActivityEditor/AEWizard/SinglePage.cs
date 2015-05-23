4917d31af18af21e5363226fa08c9ddbc62825f7
JeroenP <JeroenP@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2015-05-23 13:07:07 +0000

    https://blueprints.launchpad.net/or/+spec/remove-code-warnings: clean AEWizard regarding not overriding methods where it should
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3107 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/Contrib/ActivityEditor/AEWizard/SinglePage.cs
+++ Source/Contrib/ActivityEditor/AEWizard/SinglePage.cs
@@ -30,19 +30,49 @@ namespace AEWizard
             // Activate the page
             return true;
         }
+
+
         protected internal virtual string OnWizardBack()
         {
             // Move to the default previous page in the wizard
 
             return WizardForm.NextPage;
         }
+
+        /// <summary>
+        /// Called when the user clicks the Finish button in a wizard.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> if the wizard finishes successfully; otherwise
+        /// <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// Override this method to specify some action the user must take
+        /// when the Finish button is pressed.  Return <c>false</c> to
+        /// prevent the wizard from finishing.
+        /// </remarks>
         protected internal virtual bool OnWizardFinish()
         {
+            // Finish the wizard
             return true;
         }
 
+        /// <summary>
+        /// Called when the user clicks the Next button in a wizard.
+        /// </summary>
+        /// <returns>
+        /// <c>WizardForm.DefaultPage</c> to automatically advance to the
+        /// next page; <c>WizardForm.NoPageChange</c> to prevent the page
+        /// changing.  To jump to a page other than the next one, return
+        /// the <c>Name</c> of the page to be displayed.
+        /// </returns>
+        /// <remarks>
+        /// Override this method to specify some action the user must take
+        /// when the Next button is pressed.
+        /// </remarks>
         protected internal virtual string OnWizardNext()
         {
+            // Move to the default next page in the wizard
             return WizardForm.NextPage;
         }
 
--- Source/Contrib/ActivityEditor/AEWizard/TrainInfo.cs
+++ Source/Contrib/ActivityEditor/AEWizard/TrainInfo.cs
@@ -93,49 +93,13 @@ namespace AEWizard
         /// Override this method to specify some action the user must take
         /// when the Back button is pressed.
         /// </remarks>
-        protected internal virtual string OnWizardBack()
+        protected internal override string OnWizardBack()
         {
             // Move to the default previous page in the wizard
             Wizard.SetWizardButtons(WizardButton.Next);
             return WizardForm.NextPage;
         }
-
-        /// <summary>
-        /// Called when the user clicks the Finish button in a wizard.
-        /// </summary>
-        /// <returns>
-        /// <c>true</c> if the wizard finishes successfully; otherwise
-        /// <c>false</c>.
-        /// </returns>
-        /// <remarks>
-        /// Override this method to specify some action the user must take
-        /// when the Finish button is pressed.  Return <c>false</c> to
-        /// prevent the wizard from finishing.
-        /// </remarks>
-        protected internal virtual bool OnWizardFinish()
-        {
-            // Finish the wizard
-            return true;
-        }
-
-        /// <summary>
-        /// Called when the user clicks the Next button in a wizard.
-        /// </summary>
-        /// <returns>
-        /// <c>WizardForm.DefaultPage</c> to automatically advance to the
-        /// next page; <c>WizardForm.NoPageChange</c> to prevent the page
-        /// changing.  To jump to a page other than the next one, return
-        /// the <c>Name</c> of the page to be displayed.
-        /// </returns>
-        /// <remarks>
-        /// Override this method to specify some action the user must take
-        /// when the Next button is pressed.
-        /// </remarks>
-        protected internal virtual string OnWizardNext()
-        {
-            // Move to the default next page in the wizard
-            return WizardForm.NextPage;
-        }
+        
         #region Windows Form Designer generated code
         /// <summary>
         /// Required method for Designer support - do not modify
