da3c3d7b579e7a98b87019ab38772f350bce2a21
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2015-08-05 20:59:17 +0000

    Added section to Contribute > Developing Code about the current policies.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3209 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Website/openrails.org/contribute/developing-code/index.php
+++ Website/openrails.org/contribute/developing-code/index.php
@@ -13,8 +13,7 @@
       </div>
     </div>
  		<div class="row">
-			<div class="col-md-3"></div>
-			<div class="col-md-6">
+			<div class="col-md-12">
 
 <!-- implement an accordion -->
 <ul class="accordion">
@@ -22,7 +21,7 @@
     <a href=# id="private_changes"><h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Private Changes</h2></a>
     <div class="accordion_body">
       <p>
-Anyone can <a href="../../download/source/">download the source code</a> for Open Rails and make changes to suit themselves and create a personal version. <a href="#accessing_the_code">Instructions for this</a> are below.
+Anyone can <a href="../../download/source/">download the source code</a> for Open Rails and make changes to suit themselves and create a personal version.
       </p>
     </div>
   </li><li>
@@ -34,33 +33,31 @@ submissions and give credit in the project record for submissions that are inclu
       </p>
     </div>
   </li><li>
-    <a href=# id="source_code_repository"><h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> SVN Version Control System</h2></a>
+    <a href=# id="source_code_repository"><h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Version Control System</h2></a>
     <div class="accordion_body">
       <p>
-The Open Rails project uses Apache Subversion (often abbreviated to SVN) as the versioning and revision control system for our software.
+The Open Rails project uses Subversion (SVN) as the versioning and revision control system for our software.
 Revisions are archived in a repository and can be compared, restored and merged, so that versions are safe and several developers can work
  independently without (too much) conflict.
       </p><p>
-The Open Rails source code is managed on <a href="http://svn.uktrainsim.com/svn/openrails/trunk">our SVN server</a>:<br>
+The Open Rails source code is managed on <a href="http://svn.uktrainsim.com/svn/openrails/trunk">our Subversion server</a>.
 Read-only access is provided from the guest login:
       </p>
       <ul>
-        <li>Username = <span class="tt">orpublic</span></li>
-        <li>Password = <span class="tt">orpublic</span></li>
+        <li>Username: <span class="tt">orpublic</span></li>
+        <li>Password: <span class="tt">orpublic</span></li>
       </ul><br>
       <p>
-From your PC, you can use an SVN client such as <a href="http://tortoisesvn.net">TortoiseSVN</a> to access the files, or simply view the repository from a web browser.
+From your PC, you can use an Subversion client such as <a href="http://tortoisesvn.net">TortoiseSVN</a> to access the files, or simply view the repository from a web browser.
       </p><p>
-P.S. - Our thanks to <a href="http://www.uktrainsim.com">UKTrainSim.com</a> for donating the SVN server space and admin services.
+P.S. - Our thanks to <a href="http://www.uktrainsim.com">UKTrainSim.com</a> for donating the Subversion server space and admin services.
       </p>
     </div>
   </li><li>
     <a href=# id="accessing_the_code"><h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Accessing The Code With TortoiseSVN</h2></a>
     <div class="accordion_body">
       <p>
-If you are not familiar with SVN, please study the TortoiseSVN Help first.
-      </p><p>
-On your desktop,
+If you are not familiar with Subversion, please study the TortoiseSVN help first.
       </p>
       <ol>
         <li>From <i>Windows Explorer</i>, choose a folder to hold the project code.</li>
@@ -68,9 +65,9 @@ On your desktop,
         <li>In the <i>Checkout</i> dialog box, enter the <i>URL of repository</i> : 
           <span class="tt">http://svn.uktrainsim.com/svn/openrails/trunk</span>
         </li>
-        <li>Enter <i>Username</i> : <span class="tt">&nbsp;orpublic</span></li>
-        <li>Enter <i>Password</i> : <span class="tt">&nbsp;orpublic</span></li>
-        <li>Check that the <i>Checkout directory</i> : shows the folder where you want the files extracted to - e.g. 
+        <li>Enter username <span class="tt">&nbsp;orpublic</span></li>
+        <li>Enter password <span class="tt">&nbsp;orpublic</span></li>
+        <li>Check that the <i>Checkout directory</i> shows the folder where you want the files extracted to - e.g. 
           <span class="tt">C:\Users\Wayne\Desktop\openrails</span>
         </li>
         <li>Click OK.</li>
@@ -95,9 +92,9 @@ The main folders in the repository are:
 To compile and debug the Open Rails source code, ensure you have the following Microsoft products installed:
       </p>
       <ul>
-        <li>Visual C# 2010/12/13, any edition. The 
-          <a href="http://www.microsoft.com/en-us/download/details.aspx?id=40787">Express Edition</a> 
-         is free (Note: Install this first!)
+        <li>Visual C# 2010, 2012, 2013 - any edition, including the
+          <a href="http://www.microsoft.com/en-us/download/details.aspx?id=40787">Express Edition</a>,
+          which is free
         </li>
         <li><a href="http://www.microsoft.com/en-us/download/details.aspx?id=39">XNA Game Studio 3.1</a></li>
       </ul>
@@ -106,28 +103,28 @@ After you have downloaded the code:
       </p>
       <ol>
         <li>Open folder <i>Source</i></li>
-        <li>Double click on file <i>ORTS.sln</i> to launch Visual Studio with the Open Rails project open</li>
-        <li>From the Visual Studio menu bar, select <i>Build > Rebuild Solution</i></li>
-        <li>On the status bar (lower left), wait for the message 'Rebuild All succeeded'</li>
+        <li>Double click on file <i>ORTS.sln</i> to launch Visual Studio with the Open Rails project</li>
+        <li>From the Visual Studio menu, select <i>Build > Rebuild Solution</i></li>
+        <li>In the status bar (lower left), wait for the message 'Rebuild All succeeded'</li>
       </ol>
-      <p>The executable files have now been re-built and placed in your empty Program folder.</p>
+      <p>The executable files have now been built and placed in the <i>Program</i> folder.</p>
     </div>
   </li><li>
     <a href=# id="debugging">
-      <h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Running the RunActivity.exe code in debug mode</h2>
+      <h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Running the RunActivity.exe Code in Debug Mode</h2>
     </a>
     <div class="accordion_body">
       <p>
 Note: When debugging you will bypass the normal start menu and must specify an activity on the command line.
       </p><p>
-On the Visual Studio menu bar,
+On the Visual Studio menu,
       </p>
       <ol>
         <li>Select <i>View > Solution Explorer</i></li>
         <li>In the <i>Solution Explorer</i> box (on the right), right click on <i>RunActivity > Properties</i></li>
         <li>Use the Debug tab (on the left) to open the Debug Window.</li>
         <li>Into the "Command line arguments", enter the path of the activity that you want to run: e.g. "c:\personal\msts\routes\lps\activities\ls1.act". 
-         Use quotes to cope with spaces in the path.
+         Use quotes if the path has spaces in it.
         </li>
         <li>Below this, check the checkbox for "Enable native code debugging"</li>
         <li>Press F5 to run RunActivity.exe using your activity.</li>
@@ -137,16 +134,43 @@ On the Visual Studio menu bar,
     <a href=# id="submitting_a_change"><h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Submitting A Change</h2></a>
     <div class="accordion_body">
       <p>
-A change is best packaged as a "patch" file - a file which contains change instructions to alter the current version of Open Rails code to
-include your changes. Patches are short and readable. Also, thanks to its smart features, SVN can apply your patch successfully 
-even if some of the Open Rails code has been changed by someone else in the meantime.
+A change is best packaged as a "patch" file - a file which contains instructions to alter the current version of Open Rails code to
+include your changes. Patches are short and readable. Subversion will usually apply your patch successfully 
+even after code has been changed by someone else.
       </p><p>
-If you are offering a fix to a Bug Report, then simply attach your patch file to a post on the <a href="http://launchpad.net/or">Bug Tracker</a> explaining what you have done.
+If you are offering a fix to a Bug Report, then simply attach your patch file to the <a href="http://launchpad.net/or">Bug Tracker</a> explaining what you have done.
       </p><p>
 If you are offering an improvement or a new feature, then attach your file to a post on the Elvas Tower forum <a href="http://www.elvastower.com/forums/index.php?/forum/192-discussion/">Open Rails Discussion</a>. It would be helpful to post a message before
  you start work to give us some idea of your intentions.
       </p><p>
 We cannot promise that your changes will make it into the code, but show us what you can do and then we can talk about it.
+      </p>
+    </div>
+  </li>
+  <li>
+    <a href=# id="code_policy"><h2 class="accordion_head"><span class="glyphicon glyphicon-play btn-xs"></span> Policy for Code Changes</h2></a>
+    <div class="accordion_body">
+      <p>
+        To allow many people to contribute directly to Open Rails successfully, we have a policy in place to keep tabs on where changes come from and which changes are allowed. The policy is as follows:
+      </p>
+      <ol>
+        <li>All changes must be one of:
+          <ul>
+            <li><em>Simple</em> bug fixes, with a link to the bug</li>
+            <li><em>Targeted</em> bug fixes, with a link to the bug</li>
+            <li><em>Approved</em> blueprints, with a link to the blueprint</li>
+            <li>Documentation or localisation updates (kept separate from code changes)</li>
+          </ul>
+        </li>
+        <li><em>Simple</em> means no more than a few lines changes and no uncertainty over the logic change.</li>
+        <li><em>Targeted</em> means "Milestone" is set to the next planned stable version.</li>
+        <li><em>Approved</em> means "Direction" is "Approved", "Milestone target" is unset or set to the next planned stable version, and at least 7 days of discussion has taken place.</li>
+      </ol>
+      <p>
+        For members of the <a href="../../discover/project-team/">Development Team</a>, these policies are defined in more detail in
+        <a href="http://www.elvastower.com/forums/index.php?/topic/26392-new-policy-for-code-changes/">New policy for code changes</a>
+        and
+        <a href="http://www.elvastower.com/forums/index.php?/topic/27109-new-policy-for-blueprints/">New policy for blueprints</a>.
       </p>
     </div>
   </li>
