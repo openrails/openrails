07c9417e030fbd36c714e6f582a34e6c4bdd4c93
disc <disc@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2015-12-03 18:51:26 +0000

    More realistic twin pipe brake behavior than one in rev 3332
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3337 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/RollingStock/SubSystems/Brakes/MSTS/AirTwinPipe.cs
+++ Source/RunActivity/RollingStock/SubSystems/Brakes/MSTS/AirTwinPipe.cs
@@ -22,7 +22,6 @@ namespace ORTS
 {
     public class AirTwinPipe : AirSinglePipe
     {
-        protected float prevControlPressurePSI=0f;
         public AirTwinPipe(TrainCar car)
             : base(car)
         {
@@ -31,16 +30,16 @@ namespace ORTS
             (Car as MSTSWagon).DistributorPresent = true;
             (Car as MSTSWagon).EmergencyReservoirPresent = false;
         }
+
         public override void UpdateTripleValveState(float controlPressurePSI)
         {
-                if (controlPressurePSI < 2.2f || BrakePipePressureChanging && prevControlPressurePSI > controlPressurePSI)
-                    TripleValveState = ValveState.Release;
-                else if (BrakePipePressureChanging && prevControlPressurePSI < controlPressurePSI)
-                    TripleValveState = ValveState.Apply;
-                else
-                    TripleValveState = ValveState.Lap;
-
-                prevControlPressurePSI = controlPressurePSI;
+            if (controlPressurePSI < AutoCylPressurePSI - (TripleValveState != ValveState.Release ? 2.2f : 0f) 
+                || controlPressurePSI < 2.2f) // The latter is a UIC regulation (0.15 bar)
+                TripleValveState = ValveState.Release;
+            else if (BrakeLine3PressurePSI<1000 && controlPressurePSI > AutoCylPressurePSI + (TripleValveState != ValveState.Apply ? 2.2f : 0f))
+                TripleValveState = ValveState.Apply;
+            else
+                TripleValveState = ValveState.Lap;
         }
     }
 }
