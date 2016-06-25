f512d19a073e316ec2b454e81a9effc2ac719b15
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2016-06-25 23:08:32 +0000

    Website: Tidied up the bug reporting page and added Trello roadmap info
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3582 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Website/openrails.org/contribute/reporting-bugs/index.php
+++ Website/openrails.org/contribute/reporting-bugs/index.php
@@ -18,34 +18,36 @@
 <p>
 The Open Rails team, like all coding teams, cannot guarantee bullet-proof code. There are so many variables - so many routes and activities and locos - that our testing procedures cannot consider them all. We rely on our users to let us know when something unexpected happens.
 </p>
-<h2>Our Bug Tracker</h2>
+<h2>Bugs</h2>
 <p>
-We make good use of a neat website called <a href="http://launchpad.net/or" target="_blank">Launchpad</a> which tracks all our bugs for us, making sure that none are accidentally overlooked. 
-As soon as you file a bug report there, the team members are notified and you can discuss the bug with them. The progress of a bug is
-tracked and notified to you and in most cases a bug will progress through the stages <br><span class="tt">New > Confirmed > Triaged > Assigned > Fixed</span>
+	We use the <a href="https://bugs.launchpad.net/or" target="_blank">Launchpad bugs</a> site to track all of our bugs. After submitting a bug, our team will evaluate it and respond; you'll be kept informed as the bug is updated.
 </p>
-<h2>Suggestions</h2>
 <p>
-You can also use the Bug Tracker to send us an idea for our Wish List. Do this by submitting a bug report in the usual way and we will 
-give it the category of "Wishlist".
-</p>
-<h2>Before Using The Bug Tracker</h2>
-<p>
-Before you rush off and enter a report, do consider:
+	Before you submit a new bug, please consider:
 </p>
 <ul>
-  <li>Is your problem really caused by a bug in Open Rails?</li>
-  <li>or perhaps a fault in your model?</li>
-  <li>or perhaps a feature that Open Rails has not provided yet?</li>
+	<li>
+		Is the problem already reported? <i>Please don't submit duplicates</i> - you can use "Does this bug affect you?" to tell us that an existing bug affects more people.
+	</li>
+	<li>
+		Is the problem really caused by a bug in Open Rails? <i>Please don't submit problems with your computer or Microsoft Train Simulator</i>.
+	</li>
 </ul>
+<h2>Suggestions</h2>
 <p>
-If you are unsure, then use the forum <a href="http://www.elvastower.com/forums/index.php?/forum/244-maybe-its-a-bug/" target="_blank">Maybe It's A Bug</a> to ask for guidance. You may find that others have seen something similar or know how to correct your model.
+	We use the <a href="https://trello.com/b/DS2h3Pxc/open-rails-roadmap" target="_blank">Trello Open Rails Roadmap board</a> to track suggestions. After submitting a suggestion, our team will evaluate it and place it in the appropriate timeline. If you want to follow its progress, you'll need a Trello account and to subscribe to the card. Please note that the timelines are fluid and should only be taken as a guide.
 </p>
-<h2>Information About The Bug</h2>
 <p>
-Please attach to your report a copy of the file <span class="tt">OpenRailslog.txt</span> as this contains any error messages and lists your Open Rails settings,
-the route, activity or consist. You can find this file on your Windows Desktop. If a screenshot would help, then attach that too.
+	Before you submit a new suggestion, please consider:
 </p>
+<ul>
+	<li>
+		Is the suggestion already reported? <i>Please don't submit duplicates</i> - anyone can subscribe to and vote for any suggestions.
+	</li>
+	<li>
+		Is the suggestion really relevant to Open Rails? <i>Please don't submit suggestions for new content</i> - we don't create any content.
+	</li>
+</ul>
 			</div>
 		</div>
 <?php include "../../shared/tail.php" ?>
