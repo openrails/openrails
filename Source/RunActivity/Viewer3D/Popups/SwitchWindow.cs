dba9c69e39944b8d1bbc127261525dcb07c1ded8
carlossantucci <carlossantucci@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2016-12-06 20:46:06 +0000

    Bug fix for https://bugs.launchpad.net/or/+bug/1647844 Diverging switch direction not shown correctly for many Scalerail switches.
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3682 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/Viewer3D/Popups/SwitchWindow.cs
+++ Source/RunActivity/Viewer3D/Popups/SwitchWindow.cs
@@ -135,7 +135,7 @@ namespace Orts.Viewer3D.Popups
             var switchPreviousNodeID = Owner.Viewer.Simulator.TDB.TrackDB.TrackNodesIndexOf(SwitchPreviousNode);
             var switchBranchesAwayFromUs = SwitchNode.TrPins[0].Link == switchPreviousNodeID;
             var switchTrackSection = Owner.Viewer.Simulator.TSectionDat.TrackShapes.Get(SwitchNode.TrJunctionNode.ShapeIndex);  // TSECTION.DAT tells us which is the main route
-            var switchMainRouteIsLeft = (int)switchTrackSection.MainRoute == 0;  // align the switch
+            var switchMainRouteIsLeft = SwitchNode.TrJunctionNode.GetAngle(Owner.Viewer.Simulator.TSectionDat) > 0;  // align the switch
 
             image.Source.X = ((switchBranchesAwayFromUs == front ? 1 : 3) + (switchMainRouteIsLeft ? 1 : 0)) * SwitchImageSize;
             image.Source.Y = SwitchNode.TrJunctionNode.SelectedRoute * SwitchImageSize;
