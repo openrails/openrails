c6bc14905233d59767c64e5bd6d5ab6cc09ce361
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2016-12-13 01:00:24 +0000

    Update configuration for manual and remove DOC
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3696 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/Documentation/Manual/conf.py
+++ Source/Documentation/Manual/conf.py
@@ -53,9 +53,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'OpenRails'
-copyright = '2015, OpenRails Team'
-author = 'OpenRails Team'
+project = 'Open Rails'
+copyright = '2016 Open Rails'
+author = 'Open Rails'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -115,7 +115,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -239,8 +239,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'OpenRails.tex', 'OpenRails Documentation',
-   'OpenRails Team', 'manual'),
+  (master_doc, 'Manual.tex', 'Open Rails Manual',
+   'Open Rails', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -269,7 +269,7 @@ latex_logo = 'images/or_logo.png'
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'openrails', 'OpenRails Documentation',
+    (master_doc, 'openrails', 'Open Rails Manual',
      [author], 1)
 ]
 
@@ -283,7 +283,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'OpenRails', 'OpenRails Documentation',
+  (master_doc, 'OpenRails', 'Open Rails Manual',
    author, 'OpenRails', 'One line description of project.',
    'Miscellaneous'),
 ]
--- Source/Documentation/Manual/index.rst
+++ Source/Documentation/Manual/index.rst
@@ -1,10 +1,10 @@
-.. OpenRails documentation master file, created by
+.. Open Rails documentation master file, created by
    sphinx-quickstart on Tue Dec 22 19:39:50 2015.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-OpenRails Reference Manual
-==========================
+Open Rails Reference Manual
+===========================
 
 .. toctree::
     :maxdepth: 3
