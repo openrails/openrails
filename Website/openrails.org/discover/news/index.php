01f24b59b5bdd8b2aad8dbc3bc9c9e50483ecc7d
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2017-01-22 16:12:03 +0000

    Website: Release version 1.2
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@3764 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Website/openrails.org/discover/news/index.php
+++ Website/openrails.org/discover/news/index.php
@@ -15,9 +15,14 @@
 		<div class="row">
 			<div class="col-md-1"></div>
 			<div class="col-md-5">
+          <h2>Jan 2017 - Version 1.2</h2>
+          <p>
+            <a href="/discover/version-1-2/">Open Rails 1.2</a> released! <a href="/download/program/">Download it here</a>.
+          </p>
+          <hr />
           <h2>Mar 2016 - Version 1.1</h2>
           <p>
-            <a href="/discover/version-1-1/">Open Rails 1.1</a> released! <a href="/download/program/">Download it here</a>.
+            <a href="/discover/version-1-1/">Open Rails 1.1</a> released!
           </p>
           <hr />
           <h2>Dec 2015 - More Access to Elvas Tower</h2>
