7d8e6b4275a916ecebca6c9bab7c4d7d5689b731
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2018-10-03 20:35:36 +0000

    Contrib: fix: Don't crash if path doesn't exist
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@4245 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/Contrib/ContentManager/ContentMSTS.cs
+++ Source/Contrib/ContentManager/ContentMSTS.cs
@@ -1,17 +1,17 @@
 ﻿// COPYRIGHT 2014, 2015 by the Open Rails project.
-// 
+//
 // This file is part of Open Rails.
-// 
+//
 // Open Rails is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Open Rails is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -38,10 +38,13 @@ namespace ORTS.ContentManager
         {
             if (type == ContentType.Package)
             {
-                foreach (var item in Directory.GetDirectories(PathName))
+                if (Directory.Exists(PathName))
                 {
-                    if (ContentMSTSPackage.IsValid(item))
-                        yield return new ContentMSTSPackage(this, Path.GetFileName(item), item);
+                    foreach (var item in Directory.GetDirectories(PathName))
+                    {
+                        if (ContentMSTSPackage.IsValid(item))
+                            yield return new ContentMSTSPackage(this, Path.GetFileName(item), item);
+                    }
                 }
             }
         }
