6839de0530f7df95fecb4ec6bd5089b05e807044
twpol <twpol@a9f3cfff-6c41-7446-b0ca-052bd761e6eb>
2018-10-05 20:09:22 +0000

    http://www.elvastower.com/forums/index.php?/topic/30924-going-beyond-the-4-gb-of-memory/ Removing unnecessary reference to WPF
    git-svn-id: http://svn.uktrainsim.com/svn/openrails/trunk@4250 a9f3cfff-6c41-7446-b0ca-052bd761e6eb

--- Source/RunActivity/Viewer3D/UserInput.cs
+++ Source/RunActivity/Viewer3D/UserInput.cs
@@ -1,21 +1,21 @@
 ﻿// COPYRIGHT 2009, 2010, 2011, 2012, 2013 by the Open Rails project.
-// 
+//
 // This file is part of Open Rails.
-// 
+//
 // Open Rails is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Open Rails is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
 
-// This file is the responsibility of the 3D & Environment Team. 
+// This file is the responsibility of the 3D & Environment Team.
 
 // This checks all keys for conflicts.
 //#define CHECK_KEYMAP_DUPLICATES
@@ -33,7 +33,6 @@ using Microsoft.Xna.Framework.Input;
 using ORTS.Settings;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using System.Windows;
 using Game = Orts.Viewer3D.Processes.Game;
 
 namespace Orts.Viewer3D
@@ -63,7 +62,7 @@ namespace Orts.Viewer3D
             // Make sure we have an "idle" (everything released) keyboard and mouse state if the window isn't active.
             KeyboardState = game.IsActive ? new KeyboardState(GetKeysWithPrintScreenFix(Keyboard.GetState())) : new KeyboardState();
             MouseState = game.IsActive ? Mouse.GetState() : new MouseState(0, 0, LastMouseState.ScrollWheelValue, ButtonState.Released, ButtonState.Released, ButtonState.Released, ButtonState.Released, ButtonState.Released);
-            MouseButtonsSwapped = SystemParameters.SwapButtons;
+            MouseButtonsSwapped = System.Windows.Forms.SystemInformation.MouseButtonsSwapped;
 
 #if DEBUG_RAW_INPUT
             for (Keys key = 0; key <= Keys.OemClear; key++)
