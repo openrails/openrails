465b523d799d08cf12d7a79b226ba93a67d69df4
wacampbell <wayneinbc@gmail.com>
2019-08-05 06:20:58 -0700

    Remove unused code.

--- Architecture/CommandLog/IReceiver.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ORArchitecture.CommandLog {
-    /// <summary>
-    /// Receivers should inherit from this empty interface to be accepted by CommandLog.RegisterCommand()
-    /// </summary>
-    public interface IReceiver {
-    }
-}
