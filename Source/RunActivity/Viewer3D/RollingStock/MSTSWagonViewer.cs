46f32133bdd5efe384fbff47943870d8c6f228ae
peternewell <peter_new123@yahoo.com.au>
2019-09-04 12:33:21 +1000

    https://blueprints.launchpad.net/or/+spec/wheel-bearing-impacts - move wheel bearing temperature functionality into MS

--- Source/RunActivity/Viewer3D/RollingStock/MSTSWagonViewer.cs
+++ Source/RunActivity/Viewer3D/RollingStock/MSTSWagonViewer.cs
@@ -72,6 +72,7 @@ namespace Orts.Viewer3D.RollingStock
         List<ParticleEmitterViewer> HeatingHose = new List<ParticleEmitterViewer>();
         List<ParticleEmitterViewer> WagonSmoke = new List<ParticleEmitterViewer>();
         List<ParticleEmitterViewer> HeatingSteamBoiler = new List<ParticleEmitterViewer>();
+        List<ParticleEmitterViewer> BearingHotBox = new List<ParticleEmitterViewer>();
 
         // Create viewers for special steam effects on car
         List<ParticleEmitterViewer> WagonGenerator = new List<ParticleEmitterViewer>();
@@ -123,6 +124,15 @@ namespace Orts.Viewer3D.RollingStock
                     WagonSmoke.AddRange(emitter.Value);
 
                 foreach (var drawer in WagonSmoke)
+                {
+                    drawer.Initialize(steamTexture);
+                }
+
+                // Smoke for bearing hot box
+                if (emitter.Key.ToLowerInvariant() == "bearinghotboxfx")
+                    BearingHotBox.AddRange(emitter.Value);
+
+                foreach (var drawer in BearingHotBox)
                 {
                     drawer.Initialize(steamTexture);
                 }
@@ -485,6 +495,12 @@ namespace Orts.Viewer3D.RollingStock
                // drawer.SetOutput(car.WagonSmokeVolumeM3pS, car.WagonSmokeDurationS, car.WagonSmokeSteadyColor);
             }
 
+            // Bearing Hot box smoke
+            foreach (var drawer in BearingHotBox)
+            {
+                drawer.SetOutput(car.BearingHotBoxSmokeVelocityMpS, car.BearingHotBoxSmokeVolumeM3pS, car.BearingHotBoxSmokeDurationS, car.BearingHotBoxSmokeSteadyColor);
+            }
+
             foreach (List<ParticleEmitterViewer> drawers in ParticleDrawers.Values)
                 foreach (ParticleEmitterViewer drawer in drawers)
                     drawer.PrepareFrame(frame, elapsedTime);
@@ -693,7 +709,8 @@ namespace Orts.Viewer3D.RollingStock
                 }
             }
 
-
+            // Get the current height above "sea level" for the relevant car
+            Car.CarHeightAboveSeaLevelM = Viewer.Tiles.GetElevation(Car.WorldPosition.WorldLocation);
 
             // Control visibility of passenger cabin when inside it
             if (Viewer.Camera.AttachedCar == this.MSTSWagon
