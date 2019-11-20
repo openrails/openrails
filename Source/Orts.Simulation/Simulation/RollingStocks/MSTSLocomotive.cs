ee7d6a2dc5f57928406c50389e3cdc13129b21da
peternewell <peter_new123@yahoo.com.au>
2019-11-20 22:21:55 +1100

    Corrected issue with Traction_Braking

--- Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -3798,7 +3798,7 @@ namespace Orts.Simulation.RollingStocks
                             data = this.LocomotiveAxle.AxleForceN;
                         if (DynamicBrakePercent > 0)
                         {
-                            data = (DynamicBrakeForceN / MaxDynamicBrakeForceN) * DynamicBrakeMaxCurrentA;
+                            data = DynamicBrakeForceN;
                         }
                         data = Math.Abs(data);
                         switch (cvc.Units)
