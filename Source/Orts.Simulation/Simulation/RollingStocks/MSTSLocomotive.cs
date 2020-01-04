88555e378bebd3fe3757ce203fe8fa33c282837b
peternewell <peter_new123@yahoo.com.au>
2020-01-04 11:21:57 +1100

    Adjustment to vacuum brake to allow application of brake for combined controller

--- Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -1882,9 +1882,11 @@ namespace Orts.Simulation.RollingStocks
         /// </summary>
         protected virtual void UpdateSteamEjector(float elapsedClockSeconds)
         {
-            if (Simulator.Settings.SimpleControlPhysics || !LargeEjectorFitted) // Simple braking or when large ejector is not fitted - control Ejector automatically based upon the brake control position
+            if (Simulator.Settings.SimpleControlPhysics || !LargeEjectorFitted)
+            // Simple braking or when large ejector is not fitted - control Ejector automatically based upon the brake control position
+            // Stop ejector operation if full vacuum pressure reached
             {
-                if (TrainBrakeController.TrainBrakeControllerState == ControllerState.Release || TrainBrakeController.TrainBrakeControllerState == ControllerState.FullQuickRelease || (TrainBrakeController.TrainBrakeControllerState == ControllerState.VacContServ))
+                if ((TrainBrakeController.TrainBrakeControllerState == ControllerState.Release || TrainBrakeController.TrainBrakeControllerState == ControllerState.FullQuickRelease || (TrainBrakeController.TrainBrakeControllerState == ControllerState.VacContServ)) && (this.BrakeSystem.BrakeLine1PressurePSI > Vac.ToPress(this.TrainBrakeController.MaxPressurePSI)))
                 {
                     LargeSteamEjectorIsOn = true;  // If brake is set to a release controller, then turn ejector on
                 }
