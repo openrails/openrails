02f486a9b9678d2f6d33fbe35143c80a9057f97c
César Benito Lamata <cesarbema2009@hotmail.com>
2020-04-18 16:02:15 +0200

    Change variable name hidding field

--- Source/Orts.Simulation/Simulation/Signalling/Signals.cs
+++ Source/Orts.Simulation/Simulation/Signalling/Signals.cs
@@ -11690,9 +11690,9 @@ namespace Orts.Simulation.Signalling
 
             bool found = false;
             float distance = 0;
-            int direction = enabledTrain.TrainRouteDirectionIndex;
-            Train.TCSubpathRoute routePath = enabledTrain.Train.ValidRoute[direction];
-            int actRouteIndex = routePath == null ? -1 : routePath.GetRouteIndex(enabledTrain.Train.PresentPosition[direction].TCSectionIndex, 0);
+            int actDirection = enabledTrain.TrainRouteDirectionIndex;
+            Train.TCSubpathRoute routePath = enabledTrain.Train.ValidRoute[actDirection];
+            int actRouteIndex = routePath == null ? -1 : routePath.GetRouteIndex(enabledTrain.Train.PresentPosition[actDirection].TCSectionIndex, 0);
             if (actRouteIndex >= 0)
             {
                 float offset = 0;
