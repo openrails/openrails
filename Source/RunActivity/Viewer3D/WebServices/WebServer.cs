e66ced3dfd08f89a6902419db7c95690b927c6a4
Ryan Young <ryan@youngryan.com>
2020-07-07 00:54:29 -0700

    Rearrange the new track monitor API call in an attempt to get this PR to auto-merge with #227.

--- Source/RunActivity/Viewer3D/WebServices/WebServer.cs
+++ Source/RunActivity/Viewer3D/WebServices/WebServer.cs
@@ -216,15 +216,15 @@ namespace Orts.Viewer3D.WebServices
         #endregion
 
 
-        #region /API/TRACKMONITOR
-        [Route(HttpVerbs.Get, "/TRACKMONITOR")]
-        public Train.TrainInfo TrackMonitor() => Viewer.PlayerTrain.GetTrainInfo();
-        #endregion
-
-
         #region /API/TRACKMONITORDISPLAY
         [Route(HttpVerbs.Get, "/TRACKMONITORDISPLAY")]
         public IEnumerable<TrackMonitorDisplay.ListLabel> TrackMonitorDisplayList() => Viewer.TrackMonitorDisplayList();
+        #endregion
+
+
+        #region /API/TRACKMONITOR
+        [Route(HttpVerbs.Get, "/TRACKMONITOR")]
+        public Train.TrainInfo TrackMonitor() => Viewer.PlayerTrain.GetTrainInfo();
         #endregion
     }
 }
