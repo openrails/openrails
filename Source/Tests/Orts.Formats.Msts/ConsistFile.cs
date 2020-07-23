a6845aab64c258bc9b3aaebb5b9a9e1627d0fca6
Ryan Young <ryan@youngryan.com>
2020-07-23 12:37:44 -0700

    Add a content directory mock and an initial MSTS consist reader test.

---
+++ Source/Tests/Orts.Formats.Msts/ConsistFile.cs
@@ -0,0 +1,87 @@
+﻿// COPYRIGHT 2020 by the Open Rails project.
+// 
+// This file is part of Open Rails.
+// 
+// Open Rails is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Open Rails is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
+
+using Orts.Formats.Msts;
+using ORTS.Common;
+using System.IO;
+using Xunit;
+
+namespace Tests.Orts.Formats.Msts
+{
+    public class ConsistFileTests
+    {
+        [Fact]
+        public static void TestTrainProperties()
+        {
+            ITrainFile train;
+            using (TestContent content = new TestContent())
+                train = new ConsistFile(MakeTestFile(content));
+
+            Assert.Equal("Test consist", train.DisplayName);
+            Assert.Equal(36.65728f, train.MaxVelocityMpS);
+            Assert.Equal(0.5f, train.Durability);
+            Assert.True(train.PlayerDrivable);
+        }
+
+        private static string MakeTestFile(TestContent content)
+        {
+            const string text = @"SIMISA@@@@@@@@@@JINX0D0t______
+
+Train (
+	TrainCfg ( ""test""
+		Name(""Test consist"")
+		Serial(1)
+		MaxVelocity(36.65728 1.00000)
+		NextWagonUID(7)
+		Durability(0.50000)
+		Wagon(
+			WagonData(us2bnsfcar US2BNSFCAR)
+			UiD(0)
+		)
+		Engine(
+			UiD(1)
+			EngineData(dash9 DASH9)
+		)
+		Wagon(
+			WagonData(us2graincar US2GRAINCAR)
+			UiD(2)
+		)
+		Wagon(
+			WagonData(us2graincar US2GRAINCAR)
+			UiD(3)
+		)
+		Wagon(
+			WagonData(us2graincar US2GRAINCAR)
+			UiD(4)
+		)
+		Wagon(
+			WagonData(us2graincar US2GRAINCAR)
+			UiD(5)
+		)
+		Engine(
+			Flip()
+			UiD(6)
+			EngineData(gp38 GP38)
+		)
+	)
+)";
+            string path = Path.Combine(content.ConsistsPath, "test.con");
+            File.WriteAllText(path, text);
+            return path;
+        }
+    }
+}
---
+++ Source/Tests/TestContent.cs
@@ -0,0 +1,58 @@
+﻿// COPYRIGHT 2020 by the Open Rails project.
+// 
+// This file is part of Open Rails.
+// 
+// Open Rails is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Open Rails is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using System.Linq;
+using static System.IO.Path;
+
+namespace Tests
+{
+    public class TestContent : IDisposable
+    {
+        private bool Disposed = false;
+
+        public string Path { get; }
+
+        public string TrainsPath { get => Mkdir("trains", "lists"); }
+
+        public string ConsistsPath { get => Mkdir("trains", "consists"); }
+
+        public string TrainsetPath { get => Mkdir("trains", "trainset"); }
+
+        public string RoutesPath { get => Mkdir("routes"); }
+
+        public string SoundPath { get => Mkdir("sound"); }
+
+        public TestContent() => Path = Combine(GetTempPath(), GetRandomFileName());
+
+        private string Mkdir(params string[] subPath)
+        {
+            string path = Combine(subPath.Prepend(Path).ToArray());
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            return path;
+        }
+
+        public void Dispose()
+        {
+            if (!Disposed)
+                Directory.Delete(Path, true);
+            Disposed = true;
+        }
+    }
+}
