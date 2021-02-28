e29425459fdc267646ba4aecf30653646fea87bc
Ryan Young <ryan@youngryan.com>
2021-02-28 13:26:58 -0800

    Add explanatory note courtesy of Cesar.

--- Source/Orts.Simulation/MultiPlayer/Message.cs
+++ Source/Orts.Simulation/MultiPlayer/Message.cs
@@ -3574,6 +3574,7 @@ namespace Orts.MultiPlayer
                         else if (drawstate2 > 0) { sigHead.state = MstsSignalAspect.APPROACH_2; }
                         else { sigHead.state = MstsSignalAspect.APPROACH_3; }
                         sigHead.draw_state = sigHead.def_draw_state(sigHead.state);
+                        // Clear the text aspect so as not to leave C# scripted signals in an inconsistent state.
                         sigHead.TextSignalAspect = "";
                     }
                     break;
