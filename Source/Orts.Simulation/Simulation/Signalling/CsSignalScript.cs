40f2354f797e157c51dd32f814e68fd60d64d2cb
Ryan Young <ryan@youngryan.com>
2021-02-28 12:55:13 -0800

    Prefer TryGetValue over ContainsKey.

--- Source/Orts.Simulation/Simulation/Signalling/CsSignalScript.cs
+++ Source/Orts.Simulation/Simulation/Signalling/CsSignalScript.cs
@@ -98,7 +98,7 @@ namespace Orts.Simulation.Signalling
         /// </summary>
         /// <param name="name">Name of the draw state as defined in sigcfg</param>
         /// <returns>The index of the draw state, -1 if no one exist with that name</returns>
-        public int GetDrawState(string name) => SignalHead.signalType.DrawStates.ContainsKey(name) ? SignalHead.signalType.DrawStates[name].Index : -1;
+        public int GetDrawState(string name) => SignalHead.signalType.DrawStates.TryGetValue(name, out SignalDrawState drawState) ? drawState.Index : -1;
         /// <summary>
         /// Signal identity of this signal
         /// </summary>
