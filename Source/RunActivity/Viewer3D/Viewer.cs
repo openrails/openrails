5615bcb033b6a34b8a0086a90d26394e57be9641
Ryan Young <ryan@youngryan.com>
2021-04-20 09:27:52 -0700

    prefer pattern matching

--- Source/RunActivity/Viewer3D/Viewer.cs
+++ Source/RunActivity/Viewer3D/Viewer.cs
@@ -1355,8 +1355,7 @@ namespace Orts.Viewer3D
                 {
                     foreach (var controlRenderer in (PlayerLocomotiveViewer as MSTSLocomotiveViewer)._CabRenderer.ControlMap.Values)
                     {
-                        ICabViewMouseControlRenderer mouseRenderer = controlRenderer as ICabViewMouseControlRenderer;
-                        if (mouseRenderer != null && mouseRenderer.IsMouseWithin())
+                        if (controlRenderer is ICabViewMouseControlRenderer mouseRenderer && mouseRenderer.IsMouseWithin())
                         {
                             MouseChangingControl = mouseRenderer;
                             break;
@@ -1384,8 +1383,7 @@ namespace Orts.Viewer3D
                 {
                     foreach (var controlRenderer in (PlayerLocomotiveViewer as MSTSLocomotiveViewer)._CabRenderer.ControlMap.Values)
                     {
-                        ICabViewMouseControlRenderer mouseRenderer = controlRenderer as ICabViewMouseControlRenderer;
-                        if (mouseRenderer != null && mouseRenderer.IsMouseWithin())
+                        if (controlRenderer is ICabViewMouseControlRenderer mouseRenderer && mouseRenderer.IsMouseWithin())
                         {
                             MousePickedControl = mouseRenderer;
                             break;
