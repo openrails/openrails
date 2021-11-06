4d1dbf724a59dcf0c1e7dc15ceee10d47c65b724
Cédric GNIEWEK <gniewek.cedric@free.fr>
2021-11-06 09:56:07 +0100

    Moved signals initialization after world info addition

--- Source/Orts.Simulation/Simulation/Signalling/SignalObject.cs
+++ Source/Orts.Simulation/Simulation/Signalling/SignalObject.cs
@@ -1897,6 +1897,14 @@ namespace Orts.Simulation.Signalling
             }
         }
 
+        public void Initialize()
+        {
+            foreach (SignalHead head in SignalHeads)
+            {
+                head.Initialize();
+            }
+        }
+
         /// <summary>
         /// Gets the display aspect for the track monitor.
         /// </summary>
