b2affac45986a87e7c0d119ff88f7fea37b03681
Csantucci <carlosanit1@gmail.com>
2021-11-11 11:00:45 +0100

    Bug fix for https://bugs.launchpad.net/or/+bug/1950578 Dyn Brake set up state not disappearing in cab

--- Source/Orts.Simulation/Simulation/RollingStocks/TrainCar.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/TrainCar.cs
@@ -502,7 +502,7 @@ namespace Orts.Simulation.RollingStocks
             }
         }
 
-        public float LocalDynamicBrakePercent;
+        public float LocalDynamicBrakePercent = -1;
         public float DynamicBrakePercent
         {
             get
@@ -526,6 +526,8 @@ namespace Orts.Simulation.RollingStocks
                 if (AcceptMUSignals && Train != null)
                     Train.MUDynamicBrakePercent = value;
                 else
+                    LocalDynamicBrakePercent = value;
+                if (Train != null && this == Train.LeadLocomotive)
                     LocalDynamicBrakePercent = value;
             }
         }
