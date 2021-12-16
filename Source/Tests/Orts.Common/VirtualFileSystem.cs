ebb3e0b8ad85899200269aee799834b55aabb9c4
Péter Gulyás <gulyasp@olajterv.hu>
2021-12-16 15:02:42 +0100

    More tests and adjustments

--- Source/Tests/Orts.Common/VirtualFileSystem.cs
+++ Source/Tests/Orts.Common/VirtualFileSystem.cs
@@ -18,6 +18,7 @@
 using ORTS.Common;
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,6 +28,71 @@ namespace Tests.Orts.Common
 {
     public class VirtualFileSystem
     {
+        [Theory]
+        [InlineData("/")]
+        [InlineData(@"\")]
+        [InlineData("/MSTS")]
+        [InlineData("//MSTS//")]
+        [InlineData(@"\MSTS\")]
+        [InlineData(@"\\MSTS\\")]
+        [InlineData(Vfs.MstsBasePath)]
+        [InlineData(Vfs.ExecutablePath)]
+        public static void DirectoryExists(string dir)
+        {
+            using (var testFile = new TestFile("abc"))
+            {
+                AssertWarnings.Expected();
+                Assert.True(Vfs.Initialize(Path.GetDirectoryName(testFile.FileName), AppDomain.CurrentDomain.BaseDirectory));
+                Assert.True(Vfs.DirectoryExists(dir));
+            }
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public static void DirectoryExistsFalse(string dir)
+        {
+            using (var testFile = new TestFile("abc"))
+            {
+                AssertWarnings.Expected();
+                Assert.True(Vfs.Initialize(Path.GetDirectoryName(testFile.FileName), null));
+                Assert.False(Vfs.DirectoryExists(dir));
+            }
+        }
+
+        [Theory]
+        [InlineData("/MSTS/")]
+        [InlineData("//MSTS//")]
+        [InlineData(@"\MSTS\")]
+        [InlineData(@"\\MSTS\\")]
+        [InlineData("/MSTS/qwerty uiop/../")]
+        [InlineData(@"\MSTS\asdfgéáűő 5tre/,#.\..\.\..\")]
+        public static void FileExists(string file)
+        {
+            using (var testFile = new TestFile("abc"))
+            {
+                AssertWarnings.Expected();
+                Assert.True(Vfs.Initialize(Path.GetDirectoryName(testFile.FileName), null));
+                Assert.True(Vfs.FileExists($"{file}{Path.GetFileName(testFile.FileName)}"));
+            }
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(@"\")]
+        [InlineData("/")]
+        [InlineData("/MSTS")]
+        public static void FileExistsFalse(string file)
+        {
+            using (var testFile = new TestFile("abc"))
+            {
+                AssertWarnings.Expected();
+                Assert.True(Vfs.Initialize(Path.GetDirectoryName(testFile.FileName), null));
+                Assert.False(Vfs.FileExists(file));
+            }
+        }
+
         [Fact]
         public static void SystemFiles()
         {
@@ -37,27 +103,9 @@ namespace Tests.Orts.Common
                 var directory = Path.GetDirectoryName(file.FileName);
                 var testDir = $"{fileName}.testdir";
                 Directory.CreateDirectory(Path.Combine(directory, testDir));
-                
+
                 AssertWarnings.Expected();
-                Vfs.Initialize(directory, AppDomain.CurrentDomain.BaseDirectory);
-
-                Assert.False(Vfs.DirectoryExists(null));
-                Assert.False(Vfs.DirectoryExists(""));
-                Assert.True(Vfs.DirectoryExists("/"));
-                Assert.True(Vfs.DirectoryExists(@"\"));
-                Assert.True(Vfs.DirectoryExists("/MSTS"));
-                Assert.True(Vfs.DirectoryExists(@"\MSTS\"));
-                Assert.True(Vfs.DirectoryExists(Vfs.MstsBasePath));
-                Assert.True(Vfs.DirectoryExists(Vfs.ExecutablePath));
-
-                Assert.False(Vfs.FileExists(null));
-                Assert.False(Vfs.FileExists(""));
-                Assert.False(Vfs.FileExists("/"));
-                Assert.False(Vfs.FileExists(@"\"));
-                Assert.True(Vfs.FileExists($"/MSTS/{fileName}"));
-                Assert.True(Vfs.FileExists($@"\MSTS\{fileName}"));
-                Assert.True(Vfs.FileExists($"/MSTS/qwerty uiop/../{fileName}"));
-                Assert.True(Vfs.FileExists($@"\MSTS\asdfgéáűő 5tre/,#.\..\.\..\{fileName}"));
+                Assert.True(Vfs.Initialize(directory, null));
 
                 Assert.Equal(File.GetLastWriteTime(file.FileName), Vfs.GetLastWriteTime($"/MSTS/{fileName}"));
 
@@ -84,38 +132,88 @@ namespace Tests.Orts.Common
             }
         }
 
+        [Fact]
+        public static void ZipFiles()
+        {
+            using (var file = new TestFile("", ".zip"))
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var directory = Path.GetDirectoryName(file.FileName);
+                var contents = "Árvíztűrő tükörfúrógép";
+                var dir1 = @"1stdir";
+                var dir2 = @"2nddir";
+                string file1Name, file2Name;
+
+                using (var file1 = new TestFile(contents))
+                using (var file2 = new TestFile(contents))
+                using (var archive = ZipFile.Open(file.FileName, ZipArchiveMode.Update))
+                {
+                    file1Name = Path.GetFileName(file1.FileName);
+                    file2Name = Path.GetFileName(file2.FileName);
+                    archive.CreateEntryFromFile(file1.FileName, $@"{dir1}\{dir2}\{file1Name}");
+                    archive.CreateEntryFromFile(file2.FileName, $@"{dir1}\{dir2}\{file2Name}");
+                }
+
+                AssertWarnings.Expected();
+                Assert.True(Vfs.Initialize(directory, null));
+
+                Assert.True(Vfs.DirectoryExists($"/MSTS/{dir1}/"));
+                Assert.True(Vfs.DirectoryExists($"/MSTS/{dir1}/{dir2}"));
+                Assert.True(Vfs.FileExists($"/MSTS/{dir1}/{dir2}/{file1Name}"));
+
+                using (var archive = ZipFile.Open(file.FileName, ZipArchiveMode.Read))
+                    Assert.Equal(archive.GetEntry($@"{dir1}\{dir2}\{file1Name}").LastWriteTime.DateTime, Vfs.GetLastWriteTime($"/MSTS/{dir1}/{dir2}/{file1Name}"));
+                using (var stream = Vfs.OpenText($"/MSTS/{dir1}/{dir2}/{file2Name}"))
+                    Assert.Equal(contents, stream.ReadToEnd());
+                using (var reader = Vfs.StreamReader($"/MSTS/{dir1}/{dir2}/{file1Name}", Encoding.UTF8))
+                    Assert.Equal(contents, reader.ReadToEnd());
+                using (var reader = Vfs.StreamReader($"/MSTS/{dir1}/{dir2}/{file1Name}", Encoding.ASCII))
+                    Assert.NotEqual(contents, reader.ReadToEnd());
+
+                Assert.DoesNotContain($"/MSTS/{dir1}/{dir2}/{file2Name}".ToUpper(), Vfs.GetFiles("/MSTS/", "*.*"));
+                Assert.Contains($"/MSTS/{dir1}/{dir2}/{file2Name}".ToUpper(), Vfs.GetFiles("/MSTS/", "*.*", SearchOption.AllDirectories));
+
+                Assert.DoesNotContain($"/MSTS/{dir1}/{dir2}".ToUpper(), Vfs.GetDirectories("/MSTS/"));
+                Assert.Contains($"/MSTS/{dir1}/{dir2}".ToUpper(), Vfs.GetDirectories(@"\MSTS/", SearchOption.AllDirectories));
+
+                Assert.Throws<FileNotFoundException>(() => Vfs.FileDelete($"/MSTS/{dir1}/{dir2}/{file2Name}"));
+                Assert.Throws<DirectoryNotFoundException>(() => Vfs.OpenCreate($"/MSTS/{dir1}/{dir2}/somefile"));
+            }
+        }
+
         [Theory]
         [InlineData(@"""C:\My Routes\USA85\""", "/MSTS/ROUTES/USA85/", "#comment")]
         [InlineData(@"C:\TEMP\MSTS1.2.zip", "/MSTS/", "")]
-        [InlineData(@"C:\routes.zip\USA3\", "/msts/routes/usa3/", "# this is a comment")]
+        [InlineData(@"C:\routes.zip\USA3\", "/msts/routes/usa3/", " \t # this is a \t comment")]
         public static void ConfigFileParse(string path, string mountpoint, string comment)
         {
             using (var file = new TestFile($"{path} {mountpoint} {comment}"))
             {
                 path = path.Replace(@"\", @"\\").Replace(@"""", "");
                 mountpoint = mountpoint.ToUpper();
-                AssertWarnings.Matching($"{path} => {mountpoint}", () => Vfs.Initialize(file.FileName, null));
+                AssertWarnings.Matching($"{path} => {mountpoint}", () => Assert.False(Vfs.Initialize(file.FileName, null)));
             }
         }
 
         [Theory]
         [InlineData(@"C:\My Routes\USA85\", "/MSTS/ROUTES/USA85/", "# source path with spaces must be quoted")]
-        public static void ConfigFileParseFail1(string path, string mountpoint, string comment)
+        [InlineData(@"C:\\double.zip\\USA3\\", "//msts//routes//usa2//", " double slashes")]
+        public static void ConfigFileParseReject1(string path, string mountpoint, string comment)
         {
             using (var file = new TestFile($"{path} {mountpoint} {comment}"))
             {
-                AssertWarnings.Matching($"Cannot parse", () => Vfs.Initialize(file.FileName, null));
+                AssertWarnings.Matching($"Cannot parse", () => Assert.False(Vfs.Initialize(file.FileName, null)));
             }
         }
 
         [Theory]
         [InlineData(@"C:\TEMP\MSTS1.2.zip", "/MSTS/ROUTES", "# mount point must end with slash")]
         [InlineData(@"A", "/MSTS/ROUTES", "# mount point must end with slash")]
-        public static void ConfigFileParseFail2(string path, string mountpoint, string comment)
+        public static void ConfigFileParseReject2(string path, string mountpoint, string comment)
         {
             using (var file = new TestFile($"{path} {mountpoint} {comment}"))
             {
-                AssertWarnings.Matching($"slash", () => Vfs.Initialize(file.FileName, null));
+                AssertWarnings.Matching($"slash", () => Assert.False(Vfs.Initialize(file.FileName, null)));
             }
         }
     }
