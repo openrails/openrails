d85b4ed65a6b81670bcbfdb05d570a41adf01bce
Cédric GNIEWEK <gniewek.cedric@free.fr>
2021-12-22 19:47:52 +0100

    Dispose unmarked shapes' buffers

--- Source/RunActivity/Viewer3D/Shapes.cs
+++ Source/RunActivity/Viewer3D/Shapes.cs
@@ -101,7 +101,10 @@ namespace Orts.Viewer3D
         public void Sweep()
         {
             foreach (var path in ShapeMarks.Where(kvp => !kvp.Value).Select(kvp => kvp.Key))
+            {
+                Shapes[path].Dispose();
                 Shapes.Remove(path);
+            }
         }
 
         [CallOnThread("Updater")]
@@ -1338,7 +1341,7 @@ namespace Orts.Viewer3D
         }
     }
 
-    public class ShapePrimitive : RenderPrimitive
+    public class ShapePrimitive : RenderPrimitive, IDisposable
     {
         public Material Material { get; protected set; }
         public int[] Hierarchy { get; protected set; } // the hierarchy from the sub_object
@@ -1388,6 +1391,13 @@ namespace Orts.Viewer3D
         public virtual void Mark()
         {
             Material.Mark();
+        }
+
+        public void Dispose()
+        {
+            VertexBuffer.Dispose();
+            IndexBuffer.Dispose();
+            PrimitiveCount = 0;
         }
     }
 
@@ -1530,7 +1540,7 @@ namespace Orts.Viewer3D
     }
 #endif
 
-    public class SharedShape
+    public class SharedShape : IDisposable
     {
         static List<string> ShapeWarnings = new List<string>();
 
@@ -1663,7 +1673,7 @@ namespace Orts.Viewer3D
             }
         }
 
-        public class LodControl
+        public class LodControl : IDisposable
         {
             public DistanceLevel[] DistanceLevels;
 
@@ -1682,11 +1692,21 @@ namespace Orts.Viewer3D
             internal void Mark()
             {
                 foreach (var dl in DistanceLevels)
+                {
                     dl.Mark();
+                }
+            }
+
+            public void Dispose()
+            {
+                foreach (var dl in DistanceLevels)
+                {
+                    dl.Dispose();
+                }
             }
         }
 
-        public class DistanceLevel
+        public class DistanceLevel : IDisposable
         {
             public float ViewingDistance;
             public float ViewSphereRadius;
@@ -1721,11 +1741,21 @@ namespace Orts.Viewer3D
             internal void Mark()
             {
                 foreach (var so in SubObjects)
+                {
                     so.Mark();
+                }
+            }
+
+            public void Dispose()
+            {
+                foreach (var so in SubObjects)
+                {
+                    so.Dispose();
+                }
             }
         }
 
-        public class SubObject
+        public class SubObject : IDisposable
         {
             static readonly SceneryMaterialOptions[] UVTextureAddressModeMap = new[] {
                 SceneryMaterialOptions.TextureAddressModeWrap,
@@ -1946,7 +1976,17 @@ namespace Orts.Viewer3D
             internal void Mark()
             {
                 foreach (var prim in ShapePrimitives)
+                {
                     prim.Mark();
+                }
+            }
+
+            public void Dispose()
+            {
+                foreach (var prim in ShapePrimitives)
+                {
+                    prim.Dispose();
+                }
             }
         }
 
@@ -2174,7 +2214,17 @@ namespace Orts.Viewer3D
         {
             Viewer.ShapeManager.Mark(this);
             foreach (var lod in LodControls)
+            {
                 lod.Mark();
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var lod in LodControls)
+            {
+                lod.Dispose();
+            }
         }
     }
 
