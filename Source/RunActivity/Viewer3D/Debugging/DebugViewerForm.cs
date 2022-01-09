3a7becec0126c44d68bea174edd5e5efe613c2c3
Chris Jakeman <chris@tinyit.com>
2022-01-09 11:21:23 +0000

    Corrected zig-zag red path lines in Dispatcher Tab

--- Source/RunActivity/Viewer3D/Debugging/DebugViewerForm.cs
+++ Source/RunActivity/Viewer3D/Debugging/DebugViewerForm.cs
@@ -1154,9 +1154,10 @@ namespace Orts.Viewer3D.Debugging
 						if (stepDistance + stepLength >= initialNodeOffset && stepDistance <= initialNodeOffset + DisplayDistance)
 						{
 							var currentLocation = currentPosition.WorldLocation;
-							scaledA.X = (previousLocation.TileX * 2048 + previousLocation.Location.X - subX) * xScale; scaledA.Y = pbCanvas.Height - (previousLocation.TileZ * 2048 + previousLocation.Location.Z - subY) * yScale;
-							scaledB.X = (currentLocation.TileX * 2048 + currentLocation.Location.X - subX) * xScale; scaledB.Y = pbCanvas.Height - (currentPosition.TileZ * 2048 + currentPosition.Location.Z - subY) * yScale;
-							g.DrawLine(pathPen, scaledA, scaledB);
+							scaledA.X = (float)((previousLocation.TileX * WorldLocation.TileSize + previousLocation.Location.X - subX) * xScale); 
+							scaledA.Y = (float)(pbCanvas.Height - (previousLocation.TileZ * WorldLocation.TileSize + previousLocation.Location.Z - subY) * yScale);
+							scaledB.X = (float)((currentLocation.TileX * WorldLocation.TileSize + currentLocation.Location.X - subX) * xScale); 
+							scaledB.Y = (float)(pbCanvas.Height - (currentPosition.TileZ * WorldLocation.TileSize + currentPosition.Location.Z - subY) * yScale); g.DrawLine(pathPen, scaledA, scaledB);
 						}
 					}
 					lastObjDistance = obj.Distance;
