3e4691966f78da778ebef1253678caa921f1a310
mbm-OR <mauribachi@gmail.com>
2022-02-10 16:02:09 +0100

    Fix for https://bugs.launchpad.net/or/+bug/1960519. Crash, diesel locomotive with vacuum brakes.

--- Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/MSTSDieselLocomotive.cs
@@ -1056,7 +1056,8 @@ namespace Orts.Simulation.RollingStocks
             {
                 var indexIni = trainBrakeStatus.IndexOf(tokenIni) + tokenIni.Length + 1;
                 var indexEnd = trainBrakeStatus.IndexOf(tokenEnd) - indexIni;
-                brakeInfoValue = trainBrakeStatus.Substring(indexIni, indexEnd).TrimEnd();
+                if (indexEnd > 0)// BP found before EOT
+                    brakeInfoValue = trainBrakeStatus.Substring(indexIni, indexEnd).TrimEnd();
             }
             return brakeInfoValue;
         }
