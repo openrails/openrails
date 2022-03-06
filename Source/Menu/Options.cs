7256d3937f8bf27c66f54222d28c156e26549eeb
Chris Jakeman <chris@tinyit.com>
2022-03-06 15:29:39 +0000

    Removed changes left over

--- Source/Menu/Options.cs
+++ Source/Menu/Options.cs
@@ -189,8 +189,6 @@ namespace ORTS
             numericAdhesionMovingAverageFilterSize.Value = Settings.AdhesionMovingAverageFilterSize;
             checkBreakCouplers.Checked = Settings.BreakCouplers;
             checkCurveSpeedDependent.Checked = Settings.CurveSpeedDependent;
-            checkTunnelResistanceDependent.Checked = Settings.TunnelResistanceDependent;
-            checkWindResistanceDependent.Checked = Settings.WindResistanceDependent;
             checkHotStart.Checked = Settings.HotStart;
             checkForcedRedAtStationStops.Checked = !Settings.NoForcedRedAtStationStops;
             checkDoorsAITrains.Checked = Settings.OpenDoorsInAITrains;
@@ -467,8 +465,6 @@ namespace ORTS
             Settings.AdhesionMovingAverageFilterSize = (int)numericAdhesionMovingAverageFilterSize.Value;
             Settings.BreakCouplers = checkBreakCouplers.Checked;
             Settings.CurveSpeedDependent = checkCurveSpeedDependent.Checked;
-            Settings.TunnelResistanceDependent = checkTunnelResistanceDependent.Checked;
-            Settings.WindResistanceDependent = checkWindResistanceDependent.Checked;
             Settings.HotStart = checkHotStart.Checked;
             Settings.NoForcedRedAtStationStops = !checkForcedRedAtStationStops.Checked;
             Settings.OpenDoorsInAITrains = checkDoorsAITrains.Checked;
