ae3bf8ed1e538f406fc45bbd73c42d7b28f51bd7
Chris Jakeman <chris@tinyit.com>
2022-05-28 16:59:42 +0100

    first draft webpages added

---
+++ Website/openrails.org/discover/version-1-5/index.php
@@ -0,0 +1,62 @@
+<?php include "../../shared/head_notrack.php" ?>
+    <link rel="stylesheet" href="../../shared/iframe/iframe.css" type="text/css" />
+  </head>
+
+  <body>
+    <div class="container"><!-- Centres content and sets fixed width to suit device -->
+<?php include "../../shared/banners/choose_banner.php" ?>
+<?php include "../../shared/banners/show_banner.php" ?>
+<?php include "../../shared/menu.php" ?>
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Discover > Version 1.5</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+          <h2>New in Open Rails 1.5 (since 1.4)</h2>
+          <p>
+            A summary of the new and improved features can be found below. 
+          </p><p>
+            <a href="https://launchpad.net/or/+milestone/1.4">Important bugs</a> have been fixed in this release.
+            Please keep <a href="http://openrails.org/contribute/reporting-bugs/">reporting bugs and suggesting new features</a> 
+            so Open Rails can continue to improve.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+          <h3>Headlines</h3>
+          <ul>
+            <li>Container Loading and Unloading</li>
+          </ul>
+          <h3>What's been added</h3>
+          <ul>
+            <li>End Of Train (EOT) devices</li>
+            <li>Distributed Power display, in-cab and on webpage</li>
+            <li>Progress bar for timetable "pre-run"</li>
+            <li>HUD extended for diesel-mechanical loco</li>
+          </ul>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-5">
+        <h3>What's been improved</h3>
+          <ul>
+            <li>64-bit compatibility lifting the 3GB RAM limit</li>
+            <li>Better use of VRAM</li>
+          </ul>
+          <h3>Detailed changelog</h3>
+          <p>
+            A <a href="https://launchpad.net/or/+milestone/1.5">full list of changes</a> is available.
+          </p>
+          <h3><a href="../version-1-4/">Changes in previous version</a></h3>
+          <p>&nbsp;</p>
+        </div>
+      </div>
+	</div>
+<?php include "../../shared/tail.php" ?>
+<?php include "../../shared/banners/preload_next_banner.php" ?>
+  </body>
+</html>
