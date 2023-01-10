a9e9512d990d1e2a2e89e953af58d22febdd6b1e
peternewell <peter_new123@yahoo.com.au>
2023-01-11 06:32:25 +1100

    Fix conflict with PR #732

--- Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -1161,7 +1161,7 @@ public List<CabView> CabViewList = new List<CabView>();
             EmergencyEngagesHorn = locoCopy.EmergencyEngagesHorn;
 
             WheelslipCausesThrottleDown = locoCopy.WheelslipCausesThrottleDown;
-            DriveWheelOnlyBrakes = locoCopy.DriveWheelOnlyBrakes;
+
             CompressorIsMechanical = locoCopy.CompressorIsMechanical;
             CompressorRestartPressurePSI = locoCopy.CompressorRestartPressurePSI;
             TrainBrakePipeLeakPSIorInHgpS = locoCopy.TrainBrakePipeLeakPSIorInHgpS;
@@ -1171,7 +1171,7 @@ public List<CabView> CabViewList = new List<CabView>();
             MainResVolumeM3 = locoCopy.MainResVolumeM3;
             MainResChargingRatePSIpS = locoCopy.MainResChargingRatePSIpS;
             BrakePipeDischargeTimeFactor = locoCopy.BrakePipeDischargeTimeFactor;
-
+            DriveWheelOnlyBrakes = locoCopy.DriveWheelOnlyBrakes;
             DynamicBrakeBlended = locoCopy.DynamicBrakeBlended;
             DynamicBrakeBlendingEnabled = locoCopy.DynamicBrakeBlendingEnabled;
             DynamicBrakeAvailable = locoCopy.DynamicBrakeAvailable;
