6d2bf0414abcab689a6639b13ba95454caab4146
roeter <r.roeterdink@planet.nl>
2023-01-21 20:20:29 +0100

    Use reverse path in TT Pool

--- Source/Orts.Simulation/Simulation/Timetables/TTTurntable.cs
+++ Source/Orts.Simulation/Simulation/Timetables/TTTurntable.cs
@@ -435,6 +435,7 @@ namespace Orts.Simulation.Timetables
                 PoolDetails newPool = new PoolDetails();
                 newPool.StoragePath = new Train.TCSubpathRoute(inf);
                 newPool.StoragePathTraveller = new Traveller(Simulatorref.TSectionDat, Simulatorref.TDB.TrackDB.TrackNodes, inf);
+                newPool.StoragePathReverseTraveller = new Traveller(Simulatorref.TSectionDat, Simulatorref.TDB.TrackDB.TrackNodes, inf);
                 newPool.StorageName = inf.ReadString();
 
                 newPool.AccessPaths = null;
@@ -524,6 +525,7 @@ namespace Orts.Simulation.Timetables
             {
                 thisStorage.StoragePath.Save(outf);
                 thisStorage.StoragePathTraveller.Save(outf);
+                thisStorage.StoragePathReverseTraveller.Save(outf);
                 outf.Write(thisStorage.StorageName);
 
                 outf.Write(thisStorage.StoredUnits.Count);
@@ -724,6 +726,7 @@ namespace Orts.Simulation.Timetables
                 exitSectionLength = CalculateVectorLength(0, lastVectorIndex - 2, lastVectorIndex, trackVectors);
                 thisPath.StoragePath[0].Direction = 1;
                 thisPath.StoragePathTraveller.Direction = Traveller.TravellerDirection.Forward;
+                thisPath.StoragePathReverseTraveller.Direction = Traveller.TravellerDirection.Backward;
             }
             else
             {
@@ -734,6 +737,7 @@ namespace Orts.Simulation.Timetables
                 exitSectionLength = CalculateVectorLength(lastVectorIndex + 2, trackVectors.Length - 1, lastVectorIndex, trackVectors);
                 thisPath.StoragePath[0].Direction = 0;
                 thisPath.StoragePathTraveller.Direction = Traveller.TravellerDirection.Backward;
+                thisPath.StoragePathReverseTraveller.Direction = Traveller.TravellerDirection.Forward;
             }
 
             float totalLength = baseLength + entrySectionLength;
