76afdfebd5a21ff8e89c24362ea7cbe2a17b4cbd
Lukas Timmermann <lukas@timmermann.space>
2023-06-24 03:24:12 +0200

    Ported Launcher to AvaloniaUI.

--- Source/Launcher/Program.cs
+++ Source/Launcher/Program.cs
@@ -1,145 +1,150 @@
-﻿// COPYRIGHT 2009, 2010, 2011, 2012, 2013 by the Open Rails project.
-// 
-// This file is part of Open Rails.
-// 
-// Open Rails is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Open Rails is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Open Rails.  If not, see <http://www.gnu.org/licenses/>.
-
-/* Open Rails Launcher
- *
- * This is the program which users launch. Its purpose is to check for the
- * required dependencies before launching the menu, so it is designed to
- * run on clean versions of Windows 7 and up
- *
- * Dependencies checked for:
- *   - Microsoft .NET Framework 4.7.2
- */
-
-using Microsoft.Win32;
+﻿using Avalonia;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Dynamic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Threading;
+using MessageBox.Avalonia.Enums;
+using Microsoft.Win32;
 
-namespace ORTS
+namespace Launcher;
+
+class Program
 {
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        new Application();
+        BuildAvaloniaApp()
+            .UsePlatformDetect()
+            .SetupWithoutStarting();
+
+        // Check for any missing components.
+        var path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        List<DependencyHint> missingDependencies = new List<DependencyHint>();
+
+        CheckNetFx(missingDependencies);
+
+        if (missingDependencies.Count > 0)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in missingDependencies)
+                builder.AppendLine(item.Name);
+
+            if (ShowDialog("Open Rails requires the following:\n\n{builder.ToString()}" +
+                "\nWhen you click OK, we will guide you to download the required software.\n" +
+                (missingDependencies.Count > 1 ? "If there are multiple items missing, you need to repeat this process until all dependencies are resolved.\n" : string.Empty) +
+                "Click Cancel to quit.",
+                    "Open Rails", ButtonEnum.OkCancel, Icon.Error) == ButtonResult.Ok)
+            {
+                DownloadDependency(missingDependencies[0]);
+            }
+            return;
+        }
+
+        List<string> missingORFiles = new List<string>();
+        CheckOR(missingORFiles, path);
+        if (missingORFiles.Count > 0)
+        {
+            ShowDialog("Open Rails",$"Open Rails is missing the following:\n\n{string.Join("\n", missingORFiles.ToArray())}\n\nPlease re-install the software.",  ButtonEnum.Ok, Icon.Error);
+            return;
+        }
+        // Default menu
+        var process = Process.Start(Path.Combine(path, "Menu.exe"));
+        process.WaitForInputIdle();
+    }
+
     internal struct DependencyHint
     {
-        public string Name;
-        public string Url;
-        public string Text;
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public string Text { get; set; }
     }
 
-    static class Program
+    private static void DownloadDependency(DependencyHint dependency)
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        ShowDialog("Open Rails", $"{dependency.Text} \n\nWhen you click OK, we will try to open a browser window pointing to the URL. ", ButtonEnum.Ok, Icon.Info);
+        Process.Start(dependency.Url);
+    }
+
+    static void CheckNetFx(List<DependencyHint> missingDependencies)
+    {
+        if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            Application.EnableVisualStyles();
-
-            // Check for any missing components.
-            var path = Path.GetDirectoryName(Application.ExecutablePath);
-            List<DependencyHint> missingDependencies = new List<DependencyHint>();
-
-            CheckNetFx(missingDependencies);
-
-            if (missingDependencies.Count > 0)
-            {
-                StringBuilder builder = new StringBuilder();
-                foreach (var item in missingDependencies)
-                    builder.AppendLine(item.Name);
-
-                if (MessageBox.Show($"{Application.ProductName} requires the following:\n\n{builder.ToString()}" +
-                    "\nWhen you click OK, we will guide you to download the required software.\n" +
-                    (missingDependencies.Count > 1 ? "If there are multiple items missing, you need to repeat this process until all dependencies are resolved.\n" : string.Empty) +
-                    "Click Cancel to quit.",
-                    Application.ProductName, MessageBoxButtons.OKCancel) == DialogResult.OK)
-                {
-                    DownloadDependency(missingDependencies[0]);
-                }
+            return;
+        }
+        
+        using (var RK = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"))
+            if ((SafeReadKey(RK, "Install", 0) == 1) && (SafeReadKey(RK, "Release", 0) >= 461808))  //https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#find-net-framework-versions-45-and-later-with-code
                 return;
-            }
 
-            List<string> missingORFiles = new List<string>();
-            CheckOR(missingORFiles, path);
-            if (missingORFiles.Count > 0)
-            {
-                MessageBox.Show($"{Application.ProductName} is missing the following:\n\n{string.Join("\n", missingORFiles.ToArray())}\n\nPlease re-install the software.", Application.ProductName);
-                return;
-            }
-            // Default menu
-            var process = Process.Start(Path.Combine(path, "Menu.exe"));
-            process.WaitForInputIdle();
-        }
-
-        private static void DownloadDependency(DependencyHint dependency)
+        missingDependencies.Add(new DependencyHint()
         {
-            Clipboard.SetText(dependency.Url);
-            MessageBox.Show($"{dependency.Text} \n\nWhen you click OK, we will try to open a browser window pointing to the URL. " +
-                "You can also open a browser window yourself now and paste the URL from clipboard (Ctrl + V).", dependency.Name);
-            Process.Start(dependency.Url);
-        }
+            Name = ("Microsoft .NET Framework 4.7.2 or later"),
+            Text = "Please go to\n https://support.microsoft.com/en-us/help/4054530/microsoft-net-framework-4-7-2-offline-installer-for-windows \nto download the installation package " +
+            "for Microsoft .NET Framework 4.7.2 and install the software.",
+            Url = "https://support.microsoft.com/en-us/help/4054530/microsoft-net-framework-4-7-2-offline-installer-for-windows"
+        });
+    }
 
-        static void CheckNetFx(List<DependencyHint> missingDependencies)
+    static void CheckOR(List<string> missingFiles, string path)
+    {
+        foreach (var file in new[] {
+            // Required libraries:
+            "GNU.Gettext.dll",
+            "GNU.Gettext.WinForms.dll",
+            "ICSharpCode.SharpZipLib.dll",
+            "DotNetZip.dll",
+            @"Native/X86/OpenAL32.dll",
+            @"Native/X64/OpenAL32.dll",
+            // Programs:
+            "Menu.exe",
+            "RunActivity.exe",
+        })
         {
-            using (var RK = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\"))
-                if ((SafeReadKey(RK, "Install", 0) == 1) && (SafeReadKey(RK, "Release", 0) >= 461808))  //https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#find-net-framework-versions-45-and-later-with-code
-                    return;
-
-            missingDependencies.Add(new DependencyHint()
-            {
-                Name = ("Microsoft .NET Framework 4.7.2 or later"),
-                Text = "Please go to\n https://support.microsoft.com/en-us/help/4054530/microsoft-net-framework-4-7-2-offline-installer-for-windows \nto download the installation package " +
-                "for Microsoft .NET Framework 4.7.2 and install the software.",
-                Url = "https://support.microsoft.com/en-us/help/4054530/microsoft-net-framework-4-7-2-offline-installer-for-windows"
-            });
-        }
-
-        static void CheckOR(List<string> missingFiles, string path)
-        {
-            foreach (var file in new[] {
-                // Required libraries:
-                "GNU.Gettext.dll",
-                "GNU.Gettext.WinForms.dll",
-                "ICSharpCode.SharpZipLib.dll",
-                "DotNetZip.dll",
-                @"Native/X86/OpenAL32.dll",
-                @"Native/X64/OpenAL32.dll",
-                // Programs:
-                "Menu.exe",
-                "RunActivity.exe",
-            })
-            {
-                if (!File.Exists(Path.Combine(path, file)))
-                    missingFiles.Add($"File '{file}'");
-            }
-        }
-
-        static int SafeReadKey(RegistryKey key, string name, int defaultValue)
-        {
-            try
-            {
-                return (int)key.GetValue(name, defaultValue);
-            }
-            catch
-            {
-                return defaultValue;
-            }
+            if (!File.Exists(Path.Combine(path, file)))
+                missingFiles.Add($"File '{file}'");
         }
     }
+
+    static int SafeReadKey(RegistryKey key, string name, int defaultValue)
+    {
+        if (!System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            throw new NotSupportedException("Windows Registry only available under Microsoft Windows!");
+        }
+
+        try
+        {
+            return (int)key.GetValue(name, defaultValue);
+        }
+        catch
+        {
+            return defaultValue;
+        }
+    }
+
+    private static ButtonResult ShowDialog(string title, string message, ButtonEnum buttonEnum, Icon icon)
+    {
+        CancellationTokenSource source = new CancellationTokenSource();
+        Task<ButtonResult> dialogTask = MessageBox.Avalonia.MessageBoxManager
+            .GetMessageBoxStandardWindow(title, message, buttonEnum, icon).Show();
+        Task.Run(() => Dispatcher.UIThread.MainLoop(source.Token), source.Token);
+        return dialogTask.Result;
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace();
 }
