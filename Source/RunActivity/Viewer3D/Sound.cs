410a585de2a21971e1a30bff8155458876a4000c
Csantucci <carlosanit1@gmail.com>
2023-07-22 17:13:59 +0200

    Fix http://www.elvastower.com/forums/index.php?/topic/37347-reproducible-audio-crash-in-unstable/page__pid__299135#entry299135

--- Source/RunActivity/Viewer3D/Sound.cs
+++ Source/RunActivity/Viewer3D/Sound.cs
@@ -1420,19 +1420,28 @@ namespace Orts.Viewer3D
                     volume *= Interpolate(x, MSTSStream.VolumeCurves[i]);
                 }
 
-            var wag = (MSTSWagon)SoundSource.Viewer.Camera.AttachedCar;
-            var soundHeardInternallyCorrection = Math.Min(wag.SoundHeardInternallyCorrection[0] + wag.SoundHeardInternallyCorrection[1], 1);
-            if (SoundSource.IsExternal && SoundSource.Viewer.Camera.Style != Camera.Styles.External && !SoundSource.IsUnattenuated)
+            if (SoundSource.Viewer.Camera.Style != Camera.Styles.External)
             {
-                if (wag == null || wag.ExternalSoundPassThruPercent == -1)
-                    volume *= Program.Viewer.Settings.ExternalSoundPassThruPercent * 0.01f + (1 - Program.Viewer.Settings.ExternalSoundPassThruPercent * 0.01f) * soundHeardInternallyCorrection;
-                else volume *= wag.ExternalSoundPassThruPercent * 0.01f + (1 - wag.ExternalSoundPassThruPercent * 0.01f) * soundHeardInternallyCorrection;
+                var wag = (MSTSWagon)SoundSource.Viewer.Camera.AttachedCar;
+                var soundHeardInternallyCorrection = Math.Min(wag.SoundHeardInternallyCorrection[0] + wag.SoundHeardInternallyCorrection[1], 1);
+                if (SoundSource.IsExternal && !SoundSource.IsUnattenuated)
+                {
+                    if (wag == null || wag.ExternalSoundPassThruPercent == -1)
+                        volume *= Program.Viewer.Settings.ExternalSoundPassThruPercent * 0.01f + (1 - Program.Viewer.Settings.ExternalSoundPassThruPercent * 0.01f) * soundHeardInternallyCorrection;
+                    else volume *= wag.ExternalSoundPassThruPercent * 0.01f + (1 - wag.ExternalSoundPassThruPercent * 0.01f) * soundHeardInternallyCorrection;
+                }
+
+                if (SoundSource.IsInternalTrackSound)
+                {
+                    if (wag?.TrackSoundPassThruPercent != -1)
+                        volume *= wag.TrackSoundPassThruPercent * 0.01f + (1 - wag.TrackSoundPassThruPercent * 0.01f) * soundHeardInternallyCorrection;
+                }
             }
 
             if (SoundSource.IsInternalTrackSound && SoundSource.Viewer.Camera.Style != Camera.Styles.External)
             {
-                if (wag?.TrackSoundPassThruPercent != -1)
-                    volume *= wag.TrackSoundPassThruPercent * 0.01f + (1 - wag.TrackSoundPassThruPercent * 0.01f) * soundHeardInternallyCorrection;
+                if (((MSTSWagon)SoundSource.Viewer.Camera.AttachedCar)?.TrackSoundPassThruPercent != -1)
+                    volume *= ((MSTSWagon)SoundSource.Viewer.Camera.AttachedCar).TrackSoundPassThruPercent * 0.01f;
             }
 
             ALSoundSource.Volume = volume;
