9df6e1ddc058785b810c6cda0c0dd8a9b07e4f85
sweiland-openrails <118388365+sweiland-openrails@users.noreply.github.com>
2023-12-03 16:05:11 +0100

    Follow the selection of the wagon on the main page

--- Source/RunActivity/Viewer3D/Cameras.cs
+++ Source/RunActivity/Viewer3D/Cameras.cs
@@ -1079,8 +1079,12 @@ namespace Orts.Viewer3D
                     }
                     else if (isVisibleTrainCarViewerOrWebpage && carPosition >= 0)
                     {
-                        SetCameraCar(trainCars[carPosition]);
-                        oldCarPosition = carPosition;
+                        if (carPosition < trainCars.Count)
+                        {
+                            // sometimes when decoupling cars the carPosition is out-of-range
+                            SetCameraCar(trainCars[carPosition]);
+                            oldCarPosition = carPosition;
+                        }
                     }
                     else
                         SetCameraCar(GetCameraCars().First());
