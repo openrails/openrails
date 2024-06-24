0d3e70b275433fe37bfcbcd271fa824b29b047f5
Csantucci <carlosanit1@gmail.com>
2024-06-24 19:20:51 +0200

    Remove remnants of player train switching

--- Source/RunActivity/Viewer3D/Popups/TrainListWindow.cs
+++ Source/RunActivity/Viewer3D/Popups/TrainListWindow.cs
@@ -91,7 +91,7 @@ namespace Orts.Viewer3D.Popups
                             line.Add(viewed = new TrainLabel(Owner.TextFontDefault.Height, line.RemainingHeight, Owner.Viewer, thisTrain, "*", LabelAlignment.Right));
                             viewed.Color = Color.Red;
                         }
-                        if (Owner.Viewer.Simulator.IsAutopilotMode)
+                        if (Owner.Viewer.Simulator.IsAutopilotMode && !Owner.Viewer.Simulator.TimetableMode)
                         {
                             number.Color = thisTrain.IsPlayable ? Color.LightGreen : Color.White;
                             name.Color = thisTrain.IsPlayable ? Color.LightGreen : Color.White;
@@ -110,7 +110,7 @@ namespace Orts.Viewer3D.Popups
                 }
 
                 // Now list static trains with loco and cab
-                if (Owner.Viewer.Simulator.IsAutopilotMode)
+                if (Owner.Viewer.Simulator.IsAutopilotMode && !Owner.Viewer.Simulator.TimetableMode)
                 {
                     foreach (var thisTrain in Owner.Viewer.Simulator.Trains)
                     {
@@ -181,9 +181,9 @@ namespace Orts.Viewer3D.Popups
             }
             if (PickedTrainFromList != null && (PickedTrainFromList == Viewer.SelectedTrain || (PickedTrainFromList.TrainType == Train.TRAINTYPE.AI_INCORPORATED && 
                 (PickedTrainFromList as AITrain).IncorporatingTrain.IsPathless && (PickedTrainFromList as AITrain).IncorporatingTrain == Viewer.SelectedTrain)) && !PickedTrainFromList.IsActualPlayerTrain &&
-                Viewer.Simulator.IsAutopilotMode && PickedTrainFromList.IsPlayable)
+                Viewer.Simulator.IsAutopilotMode && PickedTrainFromList.IsPlayable && !Viewer.Simulator.TimetableMode)
             {
-                if (UserInput.IsDown(UserCommand.GameSuspendOldPlayer))
+                if (UserInput.IsDown(UserCommand.GameSuspendOldPlayer) && !Viewer.Simulator.TimetableMode)
                     Viewer.Simulator.TrainSwitcher.SuspendOldPlayer = true;
                 //Ask for change of driven train
                 Viewer.Simulator.TrainSwitcher.SelectedAsPlayer = PickedTrainFromList;
