fecb56fa8118383341e23c26632759a9d82cd19b
César Benito Lamata <cesarbema2009@hotmail.com>
2024-07-01 09:13:56 +0200

    Null pointer check

--- Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/PowerSupplies/Controllers/VoltageSelector.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/PowerSupplies/Controllers/VoltageSelector.cs
@@ -149,7 +149,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
 
         public void Save(BinaryWriter outf)
         {
-            outf.Write(Position.Name);
+            outf.Write(Position?.Name);
         }
 
         public void Restore(BinaryReader inf)
