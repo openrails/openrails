f1bfd899c915b74366da54369a0db0ededad2173
sweiland-openrails <118388365+sweiland-openrails@users.noreply.github.com>
2024-09-28 14:33:21 +0200

    Added code for Content tab "Manually Installed"

--- Source/Menu/MainForm.cs
+++ Source/Menu/MainForm.cs
@@ -578,7 +578,7 @@ namespace ORTS
         
         void buttonDownloadContent_Click(object sender, EventArgs e)
         {
-            using (var form = new DownloadContentForm(Settings))
+            using (var form = new ContentForm(Settings))
             {
                 form.ShowDialog(this);
             }
@@ -757,7 +757,7 @@ namespace ORTS
 
                 if (!initialized && Folders.Count == 0)
                 {
-                    using (var form = new DownloadContentForm(Settings))
+                    using (var form = new ContentForm(Settings))
                     {
                         switch (form.ShowDialog(this))
                         {
