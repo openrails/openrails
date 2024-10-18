1ff6eca55b312b4c184f026d225e00a660b1c733
James Ross <git@james-ross.co.uk>
2024-10-18 20:24:51 +0100

    feat: Implement date settings for notification checks

--- Source/Menu/Notifications/NotificationManager.cs
+++ Source/Menu/Notifications/NotificationManager.cs
@@ -589,6 +589,7 @@ namespace Menu.Notifications
                 if (prop == null) return "";
                 value = prop.GetValue(value);
             }
+            if (value is DateTime dateTime) return dateTime.ToString("yyyy-MM-dd");
             return value?.ToString() ?? "";
         }
 
