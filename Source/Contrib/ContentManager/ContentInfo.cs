382ea6dd14baabbd2188a69175782b56553890f7
rwf-rr <rwfischer@hotmail.com>
2025-03-24 14:18:09 -0700

    Add TpEPA and TpEDBA, when units are imperial.
    TpEPA = Tons per Equivalent Powered Axles.
    TpEDBA = Tons per Equivalent Dynamic Brake Axles.
    
    These metrics are more accurate than HP per Ton, as they are based on
    tractive effort.
    
    The calculations are based on Union Pacific practices. UP uses a
    standard axle of 10'000 lbf tractive effort or dynamic brake force.
    For example a GP40-2 is 5.0 EPA, an SD40-2 is 7.1 EPA. Both have the
    same HP (and thus result in the same HP-per-Ton).

--- Source/Contrib/ContentManager/ContentInfo.cs
+++ Source/Contrib/ContentManager/ContentInfo.cs
@@ -195,8 +195,13 @@ namespace ORTS.ContentManager
                     details.AppendFormat("Power:\t{1}{0}", Environment.NewLine, FormatStrings.FormatPower(data.MaxPowerW, IsMetric, IsImperialBHP, IsImperialBTUpS));
                     details.AppendFormat("MaxTE:\t{1}{0}", Environment.NewLine, FormatStrings.FormatForce(data.MaxTractiveForceN, IsMetric));
                     details.AppendFormat("MaxDynBrk:\t{1}{0}", Environment.NewLine, FormatStrings.FormatForce(data.MaxDynamicBrakeForceN, IsMetric));
-                    if (!IsMetric && !IsUK) { details.AppendFormat("HPT:\t{1}{0}", Environment.NewLine, FormatHPT(data.MaxPowerW, data.MassKG)); }
-                    if (!IsMetric && !IsUK) { details.AppendFormat("TpOB:\t{1}{0}", Environment.NewLine, FormatTPOB(data.MassKG, data.NumOperativeBrakes)); }
+                    if (!IsMetric && !IsUK)
+                    {
+                        details.AppendFormat("HPT:\t{1}{0}", Environment.NewLine, FormatHPT(data.MaxPowerW, data.MassKG));
+                        details.AppendFormat("TpOB:\t{1}{0}", Environment.NewLine, FormatTPOB(data.MassKG, data.NumOperativeBrakes));
+                        details.AppendFormat("TpEPA:\t{1}{0}", Environment.NewLine, FormatTonsPerEPA(data.MassKG, data.MaxTractiveForceN));
+                        details.AppendFormat("TpEDBA:\t{1}{0}", Environment.NewLine, FormatTonsPerEDBA(data.MassKG, data.MaxDynamicBrakeForceN));
+                    }
                     details.AppendFormat("MinCouplerStrength:\t{1}{0}", Environment.NewLine, FormatStrings.FormatForce(data.MinCouplerStrengthN, IsMetric));
                     details.AppendFormat("MinDerailForce:\t{1}{0}", Environment.NewLine, FormatStrings.FormatForce(data.MinDerailForceN, IsMetric));
                     details.AppendLine();
@@ -273,9 +278,6 @@ namespace ORTS.ContentManager
         /// <summary>
         /// Calculate and format horsepower per ton for consist, rounded to one decimal.
         /// </summary>
-        /// <param name="consistPowerW"></param>
-        /// <param name="consistMassKG"></param>
-        /// <returns>horsepower-per-ton formated to one decimal.</returns>
         //TODO: implement UK and metric version
         static string FormatHPT(float consistPowerW, float consistMassKG)
         {
@@ -286,14 +288,33 @@ namespace ORTS.ContentManager
         /// <summary>
         /// Calculate and format tons per operative brake for consist, rounded to an integer.
         /// </summary>
-        /// <param name="consistMassKG"></param>
-        /// <param name="consistNumOpBrakes"></param>
-        /// <returns>tons-per-operative-brake formated to an integer.</returns>
         //TODO: implement UK and metric version
         static string FormatTPOB(float consistMassKG, float consistNumOpBrakes)
         {
             var tpob = consistNumOpBrakes > 0 ? Kg.ToTUS(consistMassKG) / consistNumOpBrakes : 0;
             return string.Format("{0:0}", tpob);
+        }
+
+        /// <summary>
+        /// Calculate and format tons per equivalent powered axle, rounded to an integer.
+        /// Based on UP convention, of 10k lbf counting as one axle.
+        /// Strictly, EPA should be defined in the eng file. May be done in future.
+        /// </summary>
+        static string FormatTonsPerEPA(float consistMassKG, float consistMaxTractiveEffortN)
+        {
+            var tpepa = consistMaxTractiveEffortN > 0 ? Kg.ToTUS(consistMassKG) / (N.ToLbf(consistMaxTractiveEffortN) / 10000f) : 0;
+            return string.Format("{0:0}", tpepa);
+        }
+
+        /// <summary>
+        /// Calculate and format tons per equivalent dynamic brake axle, rounded to an integer.
+        /// Based on UP convention, of 10k lbf counting as one axle.
+        /// Strictly, EDBA should be defined in the eng file. May be done in future.
+        /// </summary>
+        static string FormatTonsPerEDBA(float consistMassKG, float consistMaxDynamicBrakeForceN)
+        {
+            var tpedba = consistMaxDynamicBrakeForceN > 0 ? Kg.ToTUS(consistMassKG) / (N.ToLbf(consistMaxDynamicBrakeForceN) / 10000f) : 0;
+            return string.Format("{0:0}", tpedba);
         }
     }
 }
