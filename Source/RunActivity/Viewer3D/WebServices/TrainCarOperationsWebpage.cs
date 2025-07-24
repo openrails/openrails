03c6f8f69605c27a29dde869dcd460a409c5c287
sweiland-openrails <118388365+sweiland-openrails@users.noreply.github.com>
2025-07-24 14:17:52 +0200

    set SelectedCarPosition to zero when uncoupling

--- Source/RunActivity/Viewer3D/WebServices/TrainCarOperationsWebpage.cs
+++ Source/RunActivity/Viewer3D/WebServices/TrainCarOperationsWebpage.cs
@@ -31,8 +31,6 @@ using Orts.Common;
 using Orts.Simulation.RollingStocks.SubSystems.Brakes.MSTS;
 using ORTS.Scripting.Api;
 using System.IO;
-using Orts.Simulation.Physics;
-using Orts.Simulation.RollingStocks.SubSystems.Brakes;
 
 namespace Orts.Viewer3D.WebServices
 {
@@ -710,10 +708,7 @@ namespace Orts.Viewer3D.WebServices
                 TrainCarViewer.NewCarPosition = carPosition - 1;
                 if (Viewer.CarOperationsWindow.CarPosition > carPosition - 1)
                     Viewer.CarOperationsWindow.Visible = false;
-                if (Viewer.TrainCarOperationsWindow.SelectedCarPosition >= Viewer.PlayerTrain.Cars.Count())
-                {
-                    Viewer.TrainCarOperationsWindow.SelectedCarPosition = Viewer.PlayerTrain.Cars.Count - 1;
-                }
+                Viewer.TrainCarOperationsWindow.SelectedCarPosition = 0;
             }
         }
 
@@ -812,10 +807,7 @@ namespace Orts.Viewer3D.WebServices
                 TrainCarViewer.CouplerChanged = true;
                 if (Viewer.CarOperationsWindow.CarPosition > carPosition)
                     Viewer.CarOperationsWindow.Visible = false;
-                if (Viewer.TrainCarOperationsWindow.SelectedCarPosition >= Viewer.PlayerTrain.Cars.Count())
-                {
-                    Viewer.TrainCarOperationsWindow.SelectedCarPosition = Viewer.PlayerTrain.Cars.Count - 1;
-                }
+                Viewer.TrainCarOperationsWindow.SelectedCarPosition = 0;
             }
         }
 
