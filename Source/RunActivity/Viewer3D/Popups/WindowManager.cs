f017f7103770c3eb743b43333e1e051142240a6c
James Ross <git@james-ross.co.uk>
2025-09-26 20:02:46 +0100

    refactor: Use clearer method name for internal texture loading

--- Source/RunActivity/Viewer3D/Popups/WindowManager.cs
+++ Source/RunActivity/Viewer3D/Popups/WindowManager.cs
@@ -103,10 +103,10 @@ namespace Orts.Viewer3D.Popups
             }
             if (ScrollbarTexture == null)
                 // TODO: This should happen on the loader thread.
-                ScrollbarTexture = SharedTextureManager.Get(Viewer.RenderProcess.GraphicsDevice, System.IO.Path.Combine(Viewer.ContentPath, "WindowScrollbar.png"));
+                ScrollbarTexture = SharedTextureManager.LoadInternal(Viewer.RenderProcess.GraphicsDevice, System.IO.Path.Combine(Viewer.ContentPath, "WindowScrollbar.png"));
             if (LabelShadowTexture == null)
                 // TODO: This should happen on the loader thread.
-                LabelShadowTexture = SharedTextureManager.Get(Viewer.RenderProcess.GraphicsDevice, System.IO.Path.Combine(Viewer.ContentPath, "WindowLabelShadow.png"));
+                LabelShadowTexture = SharedTextureManager.LoadInternal(Viewer.RenderProcess.GraphicsDevice, System.IO.Path.Combine(Viewer.ContentPath, "WindowLabelShadow.png"));
             if (NoticeTexture == null)
             {
                 var size = 256;
